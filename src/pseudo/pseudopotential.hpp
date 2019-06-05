9bc4667e9abff8720eafa8805e9e08d0f6279508
Xavier Andrade <xavier@tddft.org>
2019-06-05 01:13:53 -0700

    Removed leftover output.

--- src/pseudo/pseudopotential.hpp
+++ src/pseudo/pseudopotential.hpp
@@ -49,27 +49,20 @@ namespace pseudo {
       if(format == pseudo::format::FILE_NOT_FOUND) throw error::FILE_NOT_FOUND;
       if(format == pseudo::format::UNKNOWN) throw error::UNKNOWN_FORMAT;
       
-      std::cout << "  <!-- SpeciesReader opening file " << filename << " -->" << std::endl;
-      
       switch(format){
       case pseudo::format::QSO:
-	std::cout << "  <!--   format: QSO -->" << std::endl;
 	pseudo_ = new pseudo::qso(filename);
 	break;
       case pseudo::format::UPF1:
-	std::cout << "  <!--   format: UPF1 -->" << std::endl;
 	pseudo_ = new pseudo::upf1(filename, /*uniform_grid = */ true);
 	break;
       case pseudo::format::UPF2:
-	std::cout << "  <!--   format: UPF2 -->" << std::endl;
 	pseudo_ = new pseudo::upf2(filename, /*uniform_grid = */ true);
 	break;
       case pseudo::format::PSML:
-	std::cout << "  <!--   format: PSML -->" << std::endl;
 	pseudo_ = new pseudo::psml(filename, /*uniform_grid = */ true);
 	break;
       case pseudo::format::PSP8:
-	std::cout << "  <!--   format: PSP8 -->" << std::endl;
 	pseudo_ = new pseudo::psp8(filename);
 	break;
       default:
@@ -82,8 +75,6 @@ namespace pseudo {
 	throw error::UNSUPPORTED_TYPE;
       }
       
-      std::cout << "  <!--   size:   " << pseudo_->size() << " -->" << std::endl;
-
       std::vector<double> grid, local_potential;
 
       pseudo_->grid(grid);
