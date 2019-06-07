7bb164b50d88a093556ce040507d575cdb89dc9c
Xavier Andrade <xavier@tddft.org>
2019-06-07 09:45:09 -0700

    Updated license notices.

--- src/ions/unitcell.hpp
+++ src/ions/unitcell.hpp
@@ -1,18 +1,16 @@
 ////////////////////////////////////////////////////////////////////////////////  
 // Copyright (c) 2013, Lawrence Livermore National Security, LLC. 
-// qb@ll:  Qbox at Lawrence Livermore
-//
-// This file is part of qb@ll.
 //
 // Produced at the Lawrence Livermore National Laboratory. 
-// Written by Erik Draeger (draeger1@llnl.gov) and Francois Gygi (fgygi@ucdavis.edu).
+// Written by Xavier Andrade <xavier@llnl.gov>, Erik Draeger
+// <draeger1@llnl.gov> and Francois Gygi <fgygi@ucdavis.edu>.
 // Based on the Qbox code by Francois Gygi Copyright (c) 2008 
 // LLNL-CODE-635376. All rights reserved. 
 //
-// qb@ll is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// This program is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,11 +1,27 @@
-#ifdef COMPILATION_INSTRUCTIONS
-c++ -O3 -I$HOME/prj/alf/boost $0 -o $0.x -lfftw3; exit;
-#endif
-//#include "../ions/geometry.hpp"
-#include "../ions/unitcell.hpp"
-#include "../basis/plane_wave.hpp"
-//#include "../hamiltonian/atomic_potential.hpp"
+/*
+ Copyright (C) 2019 Xavier Andrade, Alfredo Correa.
 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
+  
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+  
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#include <ions/geometry.hpp>
+#include <ions/unitcell.hpp>
+#include <basis/plane_wave.hpp>
+#include <ions/geometry.hpp>
+#include <hamiltonian/atomic_potential.hpp>
 #include <multi/array.hpp>
 #include <multi/adaptors/fftw.hpp>
 
