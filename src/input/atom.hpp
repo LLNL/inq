46f7aa033a78dda383c58c528c016ffeb19a805f
Xavier Andrade <xavier@tddft.org>
2019-08-14 06:35:18 -0700

    Removed unused emacs comments.

--- src/input/atom.hpp
+++ src/input/atom.hpp
@@ -141,11 +141,4 @@ TEST_CASE("class ions::atom", "[input::atom]") {
 
 #endif
 
-
 #endif
-
-// Local Variables:
-// eval:(setq indent-tabs-mode: t tab-width: 2)
-// mode: c++
-// coding: utf-8
-// End:
--- src/input/species.hpp
+++ src/input/species.hpp
@@ -189,9 +189,3 @@ TEST_CASE("class input::species", "[input::species]") {
 
 
 #endif
-
-// Local Variables:
-// eval:(setq indent-tabs-mode: t tab-width: 2)
-// mode: c++
-// coding: utf-8
-// End:
