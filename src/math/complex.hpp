5f9cc6ef9d2534ff0e6f0daae3638f755404e5b8
Xavier Andrade <xavier@tddft.org>
2019-08-31 09:13:14 -0700

    Defined the real and imag functions.

--- src/math/complex.hpp
+++ src/math/complex.hpp
@@ -14,5 +14,21 @@ inline complex conj(const complex & z){
 	return std::conj(z);
 }
 
+inline double real(const double & x){
+	return x;
+}
+
+inline double real(const complex & z){
+	return std::real(z);
+}
+
+inline double imag(const double &){
+	return 0.0;
+}
+
+inline double imag(const complex & z){
+	return std::imag(z);
+}
+
 #endif
 
