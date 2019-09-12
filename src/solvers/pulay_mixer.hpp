e0725a2a67fde62eaa7486f90003d9707229e626
Xavier Andrade <xavier@tddft.org>
2019-09-12 00:54:37 -0700

    Still-non-working solver.

--- src/solvers/pulay_mixer.hpp
+++ src/solvers/pulay_mixer.hpp
@@ -33,9 +33,7 @@ namespace solvers {
 
 		/*
 
-			The DIIS mixing of Pulay, as described in 
-			
-			Kresse and Furthmueller, Phys. Rev. B, 54 11169 (1996)
+			The DIIS mixing of Pulay, as described in Kresse and Furthmueller, Phys. Rev. B, 54 11169 (1996)
 
 		*/
 		
@@ -43,10 +41,10 @@ namespace solvers {
 
 		template <class mix_type>
     pulay_mixer(const int arg_steps, const double arg_mix_factor, const mix_type & initial_value):
-			steps_(0),
-			max_steps_(arg_steps),
-			current_(0),
-			previous_(-1),
+			size_(0),
+			max_size_(arg_steps),
+			current_(-1),
+			previous_(-2),
       mix_factor_(arg_mix_factor),
       ff_({arg_steps, initial_value.size()}),
 			dff_({arg_steps, initial_value.size()}) {
@@ -58,69 +56,80 @@ namespace solvers {
 		template <class mix_type>
     void operator()(mix_type & new_value){
 			
-			steps_ = std::min(steps_ + 1, max_steps_);
+			size_ = std::min(size_ + 1, max_size_);
 
 			current_++;
-			if(current_ > steps_) current_ = 0;
+			if(current_ >= max_size_) current_ = 0;
 
 			previous_++;
-			if(previous_ > steps_) previous_ = 0;
+			if(previous_ >= max_size_) previous_ = 0;
 
-			if(steps_ == 1){
+			if(size_ == 1){
 				//the first step we do linear mixing
 				
 				//DATAOPERATIONS
 				for(unsigned ii = 0; ii < new_value.size(); ii++){
-					dff_[current_][ii] = new_value[ii] - ff_[0][ii];
+					dff_[0][ii] = new_value[ii] - ff_[0][ii];
 					new_value[ii] = mix_factor_*new_value[ii] + (1.0 - mix_factor_)*ff_[0][ii];
+					ff_[0][ii] = new_value[ii];
 				}
 
 				return;
 			}
-			
+
+			std::cout << "size " << size_ << '\t' << current_ << '\t' << previous_<< std::endl;
+
       //DATAOPERATIONS
       for(unsigned ii = 0; ii < new_value.size(); ii++){
 				ff_[current_][ii] = new_value[ii];
 				dff_[current_][ii] = new_value[ii] - ff_[previous_][ii];
 			}
 
-			boost::multi::array<typename mix_type::value_type, 2> amatrix({steps_, steps_});
+			boost::multi::array<typename mix_type::value_type, 2> amatrix({size_, size_});
 
-			for(int ii = 0; ii < steps_; ii++){
-				for(int jj = 0; jj < steps_; jj++){
+			for(int ii = 0; ii < size_; ii++){
+				for(int jj = 0; jj < size_; jj++){
 					typename mix_type::value_type aa = 0.0;
 					for(unsigned kk = 0; kk < new_value.size(); kk++) aa += conj(dff_[ii][kk])*dff_[jj][kk];
 					amatrix[ii][jj] = aa;
 				}
 			}
 
+			std::cout << amatrix[0][0] << '\t' << amatrix[0][1] << std::endl;
+			std::cout << amatrix[1][0] << '\t' << amatrix[1][1] << std::endl;
+
+			
 			// REDUCE GRID amatrix
 
-			boost::multi::array<typename mix_type::value_type, 1> alpha(steps_, 1.0);
+			boost::multi::array<typename mix_type::value_type, 1> alpha(size_, 1.0);
 
+			std::cout << "alpha = " << alpha[0] << '\t' << alpha[1] << std::endl;
+			
 			solvers::linear_symmetric(amatrix, alpha);
+
+			std::cout << "alpha = " << alpha[0] << '\t' << alpha[1] << std::endl;
 
 			//DATAOPERATIONS
 			type sumalpha = 0.0;
-			for(int jj = 0; jj < steps_; jj++) sumalpha += alpha[jj];
+			for(int jj = 0; jj < size_; jj++) sumalpha += alpha[jj];
 			std::cout << sumalpha << std::endl;
 
-			for(int jj = 0; jj < steps_; jj++) alpha[jj] /= sumalpha;
+			for(int jj = 0; jj < size_; jj++) alpha[jj] /= sumalpha;
 			std::cout << sumalpha << std::endl;
 			
-			for(int jj = 0; jj < steps_; jj++) sumalpha += alpha[jj];
+			for(int jj = 0; jj < size_; jj++) sumalpha += alpha[jj];
 			std::cout << sumalpha << std::endl;
 		
 			//DATAOPERATIONS
       for(unsigned ii = 0; ii < new_value.size(); ii++)	new_value[ii] = 0.0;
-			for(int jj = 0; jj < steps_; jj++) for(unsigned ii = 0; ii < new_value.size(); ii++) new_value[ii] += alpha[jj]*ff_[jj][ii];
+			for(int jj = 0; jj < size_; jj++) for(unsigned ii = 0; ii < new_value.size(); ii++) new_value[ii] += alpha[jj]*ff_[jj][ii];
 			
     }
 
   private:
 
-		int steps_;
-		int max_steps_;
+		int size_;
+		int max_size_;
 		int current_;
 		int previous_;
     double mix_factor_;
