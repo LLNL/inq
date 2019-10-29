72204b1593f6ef2cf5c78610457e112d32573c21
Xavier Andrade <xavier@tddft.org>
2019-10-29 16:42:31 -0700

    Forgot to add the new file.

---
+++ src/math/array.hpp
@@ -0,0 +1,37 @@
+/* -*- indent-tabs-mode: t -*- */
+
+#ifndef MATH__ARRAY
+#define MATH__ARRAY
+
+/*
+ Copyright (C) 2019 Xavier Andrade, Alfredo Correa.
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
+  
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+  
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <multi/array.hpp>
+
+#ifdef HAVE_CUDA
+#include <multi/memory/adaptors/cuda/allocator.hpp>
+#endif
+
+namespace math {
+
+  template <class type, size_t dim>
+  using array = boost::multi::array<type, dim>;
+ 
+}
+
+#endif
