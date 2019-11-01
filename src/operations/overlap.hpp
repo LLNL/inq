52f092f2c1c47c1cb6cf654605121eecd8f29c49
Xavier Andrade <xavier@tddft.org>
2019-11-01 16:27:31 -0700

    The include config.h was missing in some files.

--- src/operations/overlap.hpp
+++ src/operations/overlap.hpp
@@ -21,6 +21,10 @@
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <math/array.hpp>
 #include <cassert>
 #include <multi/adaptors/blas.hpp>
