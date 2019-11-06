6eeb59af02cf37ce3f30b5e57896d1e85afbcb96
Xavier Andrade <xavier@tddft.org>
2019-11-05 21:06:05 -0800

    2D version of gpu::run.

--- src/utils/gpu.hpp
+++ src/utils/gpu.hpp
@@ -21,7 +21,9 @@
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+#ifdef HAVE_CUDA
 #include <cuda.h>
+#endif
 
 #define CUDA_BLOCK_SIZE 1024
 
@@ -29,20 +31,42 @@ namespace gpu {
 
 #ifdef HAVE_CUDA
   template <class kernel_type>
-  __global__ void cuda_run_kernel(long size, kernel_type kernel){
+  __global__ void cuda_run_kernel_1(unsigned size, kernel_type kernel){
     auto ii = blockIdx.x*blockDim.x + threadIdx.x;
     if(ii < size) kernel(ii);
   }
 #endif
-  
+
   template <class kernel_type>
   void run(long size, kernel_type kernel){
     
-    auto nblock = size/CUDA_BLOCK_SIZE;
-    if(nblock*CUDA_BLOCK_SIZE != size) nblock++;
+    unsigned nblock = (size + CUDA_BLOCK_SIZE - 1)/CUDA_BLOCK_SIZE;
     
 #ifdef HAVE_CUDA
-    cuda_run_kernel<<<nblock, CUDA_BLOCK_SIZE>>>(size, kernel);
+    cuda_run_kernel_1<<<nblock, CUDA_BLOCK_SIZE>>>(size, kernel);
+    
+    cudaDeviceSynchronize();
+#endif
+    
+  }
+
+#ifdef HAVE_CUDA
+  template <class kernel_type>
+  __global__ void cuda_run_kernel_2(unsigned sizex, unsigned sizey, kernel_type kernel){
+    auto ix = blockIdx.x*blockDim.x + threadIdx.x;
+    auto iy = blockIdx.y*blockDim.y + threadIdx.y;
+    if(ix < sizex && iy < sizey) kernel(ix, iy);
+  }
+#endif
+ 
+  template <class kernel_type>
+  void run(long sizex, long sizey, kernel_type kernel){
+
+    //OPTIMIZATION, this is not ideal if sizex < CUDA_BLOCK_SIZE
+    unsigned nblock = (sizex + CUDA_BLOCK_SIZE - 1)/CUDA_BLOCK_SIZE;
+    
+#ifdef HAVE_CUDA
+    cuda_run_kernel_2<<<{nblock, unsigned(sizey)}, {CUDA_BLOCK_SIZE, 1}>>>(sizex, sizey, kernel);
     
     cudaDeviceSynchronize();
 #endif
