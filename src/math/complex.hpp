b93819bd5eee3a9bbd75db76230d492c27b5e69d
Xavier Andrade <xavier@tddft.org>
2019-11-07 22:13:00 -0800

    Some cleaning of the complex code.

--- src/math/complex.hpp
+++ src/math/complex.hpp
@@ -12,13 +12,7 @@
 
 using complex = std::complex<double>;
 
-GPU_FUNCTION inline double conj(const double & x){
-	return x;
-}
-
-GPU_FUNCTION inline complex conj(const complex & z){
-	return complex(std::real(z), -std::imag(z));
-}
+//real
 
 GPU_FUNCTION inline double real(const double & x){
 	return x;
@@ -28,6 +22,8 @@ GPU_FUNCTION inline double real(const complex & z){
 	return std::real(z);
 }
 
+//imag
+
 GPU_FUNCTION inline double imag(const double &){
 	return 0.0;
 }
@@ -36,14 +32,27 @@ GPU_FUNCTION inline auto imag(const complex & z){
 	return std::imag(z);
 }
 
-GPU_FUNCTION inline auto operator*(const double x, const complex & z){
-	return complex{x*real(z), x*imag(z)};
+//norm
+
+GPU_FUNCTION inline auto norm(const double & x){
+	return x*x;
 }
 
-GPU_FUNCTION inline auto operator*(const complex & z, const double x){
-	return complex{real(z)*x, imag(z)*x};
+GPU_FUNCTION inline auto norm(const complex & z){
+	return real(z)*real(z) + imag(z)*imag(z);
 }
 
+//conj
+
+GPU_FUNCTION inline double conj(const double & x){
+	return x;
+}
+
+GPU_FUNCTION inline complex conj(const complex & z){
+	return complex(std::real(z), -std::imag(z));
+}
+
+// sum
 GPU_FUNCTION inline auto operator+(const complex & z1, const complex z2){
 	return complex{real(z1) + real(z2), imag(z1) + imag(z2)};
 }
@@ -53,21 +62,26 @@ GPU_FUNCTION inline auto & operator+=(complex & z1, const complex z2){
 	return z1;
 }
 
-GPU_FUNCTION inline auto operator/(const complex & z, const double x){
-	return complex{real(z)/x, imag(z)/x};
+// multiplication
+
+GPU_FUNCTION inline auto operator*(const double x, const complex & z){
+	return complex{x*real(z), x*imag(z)};
+}
+
+GPU_FUNCTION inline auto operator*(const complex & z, const double x){
+	return complex{real(z)*x, imag(z)*x};
 }
 
 GPU_FUNCTION inline auto operator*(const complex & z1, const complex z2){
 	return complex{real(z1)*real(z2) - imag(z1)*imag(z2), real(z1)*imag(z2) + imag(z1)*real(z2)};
 }
 
-GPU_FUNCTION inline auto norm(const double & x){
-	return x*x;
+//division
+
+GPU_FUNCTION inline auto operator/(const complex & z, const double x){
+	return complex{real(z)/x, imag(z)/x};
 }
 
-GPU_FUNCTION inline auto norm(const complex & z){
-	return real(z)*real(z) + imag(z)*imag(z);
-}
 
 #endif
 
