10e966c8b12f7503cf347146b8f127a98f104635
Xavier Andrade <xavier@tddft.org>
2020-04-23 21:26:01 -0700

    Randomize from -1 to 1 instead from 0 to 1.

--- src/operations/randomize.hpp
+++ src/operations/randomize.hpp
@@ -36,7 +36,7 @@ struct uniform_distribution<double>{// : std::uniform_real_distribution<double>{
 	template<class Generator>
 	auto operator()(Generator& g) GPU_FUNCTION {
 		static double const max = std::numeric_limits<typename Generator::result_type>::max() + 1.;
-		return g()/max;
+		return 2.0*g()/max - 1.0;
 	}
 	static constexpr std::size_t rngs_per_sample = 1;
 };
@@ -118,18 +118,18 @@ TEST_CASE("function operations::randomize", "[operations::randomize]") {
 			std::cout << norms[ist] << std::endl;
 			}*/
 
-		if(aa.set_part().contains(0))  REQUIRE(norms[aa.set_part().global_to_local(0)] == 336.099_a);
-		if(aa.set_part().contains(1))  REQUIRE(norms[aa.set_part().global_to_local(1)] == 335.697_a);
-		if(aa.set_part().contains(2))  REQUIRE(norms[aa.set_part().global_to_local(2)] == 335.101_a);
-		if(aa.set_part().contains(3))  REQUIRE(norms[aa.set_part().global_to_local(3)] == 327.385_a);
-		if(aa.set_part().contains(4))  REQUIRE(norms[aa.set_part().global_to_local(4)] == 337.327_a);
-		if(aa.set_part().contains(5))  REQUIRE(norms[aa.set_part().global_to_local(5)] == 330.692_a);
-		if(aa.set_part().contains(6))  REQUIRE(norms[aa.set_part().global_to_local(6)] == 331.003_a);
-		if(aa.set_part().contains(7))  REQUIRE(norms[aa.set_part().global_to_local(7)] == 328.333_a);
-		if(aa.set_part().contains(8))  REQUIRE(norms[aa.set_part().global_to_local(8)] == 333.662_a);
-		if(aa.set_part().contains(9))  REQUIRE(norms[aa.set_part().global_to_local(9)] == 330.545_a);
-		if(aa.set_part().contains(10)) REQUIRE(norms[aa.set_part().global_to_local(10)] == 335.836_a);
-		if(aa.set_part().contains(11)) REQUIRE(norms[aa.set_part().global_to_local(11)] == 328.899_a);
+		if(aa.set_part().contains(0))  REQUIRE(norms[aa.set_part().global_to_local(0)] == 330.1381395023_a);
+		if(aa.set_part().contains(1))  REQUIRE(norms[aa.set_part().global_to_local(1)] == 330.5444105287_a);
+		if(aa.set_part().contains(2))  REQUIRE(norms[aa.set_part().global_to_local(2)] == 331.5435469092_a);
+		if(aa.set_part().contains(3))  REQUIRE(norms[aa.set_part().global_to_local(3)] == 340.0201855451_a);
+		if(aa.set_part().contains(4))  REQUIRE(norms[aa.set_part().global_to_local(4)] == 332.959828166_a);
+		if(aa.set_part().contains(5))  REQUIRE(norms[aa.set_part().global_to_local(5)] == 333.1967703871_a);
+		if(aa.set_part().contains(6))  REQUIRE(norms[aa.set_part().global_to_local(6)] == 328.9429576951_a);
+		if(aa.set_part().contains(7))  REQUIRE(norms[aa.set_part().global_to_local(7)] == 336.8659386972_a);
+		if(aa.set_part().contains(8))  REQUIRE(norms[aa.set_part().global_to_local(8)] == 331.6228884743_a);
+		if(aa.set_part().contains(9))  REQUIRE(norms[aa.set_part().global_to_local(9)] == 334.5374311686_a);
+		if(aa.set_part().contains(10)) REQUIRE(norms[aa.set_part().global_to_local(10)] == 334.6847273315_a);
+		if(aa.set_part().contains(11)) REQUIRE(norms[aa.set_part().global_to_local(11)] == 334.1846706334_a);
 
 	}
 	
@@ -147,18 +147,18 @@ TEST_CASE("function operations::randomize", "[operations::randomize]") {
 			std::cout << std::scientific << real(norms[ist])<< std::endl;
 			}*/
 
-		if(aa.set_part().contains(0))  REQUIRE(real(norms[aa.set_part().global_to_local(0)]) == 670.4340_a);
-		if(aa.set_part().contains(1))  REQUIRE(real(norms[aa.set_part().global_to_local(1)]) == 663.3693_a);
-		if(aa.set_part().contains(2))  REQUIRE(real(norms[aa.set_part().global_to_local(2)]) == 665.3004_a);
-		if(aa.set_part().contains(3))  REQUIRE(real(norms[aa.set_part().global_to_local(3)]) == 660.0291_a);
-		if(aa.set_part().contains(4))  REQUIRE(real(norms[aa.set_part().global_to_local(4)]) == 660.9823_a);
-		if(aa.set_part().contains(5))  REQUIRE(real(norms[aa.set_part().global_to_local(5)]) == 659.2983_a);
-		if(aa.set_part().contains(6))  REQUIRE(real(norms[aa.set_part().global_to_local(6)]) == 664.7990_a);
-		if(aa.set_part().contains(7))  REQUIRE(real(norms[aa.set_part().global_to_local(7)]) == 666.0472_a);
-		if(aa.set_part().contains(8))  REQUIRE(real(norms[aa.set_part().global_to_local(8)]) == 669.8478_a);
-		if(aa.set_part().contains(9))  REQUIRE(real(norms[aa.set_part().global_to_local(9)]) == 667.2162_a);
-		if(aa.set_part().contains(10)) REQUIRE(real(norms[aa.set_part().global_to_local(10)]) == 666.8721_a);
-		if(aa.set_part().contains(11)) REQUIRE(real(norms[aa.set_part().global_to_local(11)]) == 668.4646_a);
+		if(aa.set_part().contains(0))  REQUIRE(real(norms[aa.set_part().global_to_local(0)]) == 669.1459385544_a);
+		if(aa.set_part().contains(1))  REQUIRE(real(norms[aa.set_part().global_to_local(1)]) == 664.8544595319_a);
+		if(aa.set_part().contains(2))  REQUIRE(real(norms[aa.set_part().global_to_local(2)]) == 669.6590362331_a);
+		if(aa.set_part().contains(3))  REQUIRE(real(norms[aa.set_part().global_to_local(3)]) == 668.4278672543_a);
+		if(aa.set_part().contains(4))  REQUIRE(real(norms[aa.set_part().global_to_local(4)]) == 668.0965017232_a);
+		if(aa.set_part().contains(5))  REQUIRE(real(norms[aa.set_part().global_to_local(5)]) == 665.1942561788_a);
+		if(aa.set_part().contains(6))  REQUIRE(real(norms[aa.set_part().global_to_local(6)]) == 665.6688584485_a);
+		if(aa.set_part().contains(7))  REQUIRE(real(norms[aa.set_part().global_to_local(7)]) == 672.3987040517_a);
+		if(aa.set_part().contains(8))  REQUIRE(real(norms[aa.set_part().global_to_local(8)]) == 671.7988968555_a);
+		if(aa.set_part().contains(9))  REQUIRE(real(norms[aa.set_part().global_to_local(9)]) == 672.6486737858_a);
+		if(aa.set_part().contains(10)) REQUIRE(real(norms[aa.set_part().global_to_local(10)]) == 664.3526206348_a);
+		if(aa.set_part().contains(11)) REQUIRE(real(norms[aa.set_part().global_to_local(11)]) == 674.5678452953_a);
 
 	}
 
