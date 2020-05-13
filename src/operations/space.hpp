2741e668b4f477cd3b27b06d1d0401d4d2b670ad
Alfredo Correa <alfredo.correa@gmail.com>
2020-05-13 15:49:45 -0700

    remove hard dependency on cuda cufft

--- src/operations/space.hpp
+++ src/operations/space.hpp
@@ -24,14 +24,14 @@
 #include <gpu/run.hpp>
 #include <basis/field_set.hpp>
 
-#if 0
+#if 1
 #include <multi/adaptors/fftw.hpp>
 
 #ifdef HAVE_CUDA
 #include <multi/adaptors/cufft.hpp>
 #endif
 #endif
-#include <multi/adaptors/fft.hpp>
+//#include <multi/adaptors/fft.hpp>
 
 #include <cassert>
 
