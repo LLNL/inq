7e801b81c662e18f963696ada1a714e67f82700f
Alfredo Correa <correaa@cuk.attlocal.net>
2020-05-17 13:43:11 -0700

    remove dependency con config.h
    there is nothing to be configured

--- src/math/vec3d.hpp
+++ src/math/vec3d.hpp
@@ -23,10 +23,6 @@
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
 #include <iostream>
 #include <cmath>
 #include <cassert>
