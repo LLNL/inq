82f272471be6ee217cf21da0c651a92a06816cd9
Xavier Andrade <xavier@tddft.org>
2020-05-26 22:49:03 -0700

    Code style and cleaning.

--- src/operations/gradient.hpp
+++ src/operations/gradient.hpp
@@ -25,31 +25,39 @@
 #include <cassert>
 
 namespace operations {
-	auto gradient(basis::field<basis::fourier_space, complex> const & ff){		// Gradient function for the field type defined in the Fourier space which return a filed-set 'grad'
+
+	auto gradient(basis::field<basis::fourier_space, complex> const & ff){
 		basis::field_set<basis::fourier_space, complex> grad(ff.basis(), 3);
-		for(int ix = 0; ix < ff.basis().sizes()[0]; ix++){ 			// Iterating over each x-,y- and z- components of the input field 
+
+		for(int ix = 0; ix < ff.basis().sizes()[0]; ix++){
 			for(int iy = 0; iy < ff.basis().sizes()[1]; iy++){
 				for(int iz = 0; iz < ff.basis().sizes()[2]; iz++){
 					auto gvec = ff.basis().gvector(ix, iy, iz);
-					// Iterating over each vectorial components of grad field-set and corresponding G-vector at (ix,iy,iz) point in the space
 					for(int idir = 0; idir < 3 ; idir++) grad.cubic()[ix][iy][iz][idir] = complex(0.0, 1.0)*gvec[idir]*ff.cubic()[ix][iy][iz]; 
 				}
 			}
 		}
 		return grad;
 	}
-	auto gradient(basis::field<basis::real_space, complex> const & ff){		// Gradient function for the field type (complex) defined in the Real space which return a filed-set 'grad_real'
-		auto ff_fourier = operations::space::to_fourier(ff); 			// Tranform input field to Fourier space
-		auto grad_fourier = gradient(ff_fourier); 				// To calculate the gradient in Fourier space with the use of the above-defined function 'gradient'
-		auto grad_real = operations::space::to_real(grad_fourier); 		// Transform output field-set to Real space (complex)
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	auto gradient(basis::field<basis::real_space, complex> const & ff){
+		auto ff_fourier = operations::space::to_fourier(ff);
+		auto grad_fourier = gradient(ff_fourier);
+		auto grad_real = operations::space::to_real(grad_fourier);
 		return grad_real;
 	}
-	auto gradient(basis::field<basis::real_space, double> const & ff){		// Gradient function for the field type (double) defined in the Real space which return a filed-set 'grad_real'
-		auto ff_fourier = operations::space::to_fourier(ff); 			// Tranform input field to Fourier space
-		auto grad_fourier = gradient(ff_fourier); 				// To calculate the gradient in Fourier space with the use of the above-defined function 'gradient'
-		auto grad_real = operations::space::to_real(grad_fourier); 		// Transform output field-set to Real space (double)
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	auto gradient(basis::field<basis::real_space, double> const & ff){
+		auto ff_fourier = operations::space::to_fourier(ff);
+		auto grad_fourier = gradient(ff_fourier);
+		auto grad_real = operations::space::to_real(grad_fourier);
 		return grad_real.real();
 	}
+	
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -61,24 +69,19 @@ namespace operations {
 #include <catch2/catch.hpp>
 #include <math/vec3d.hpp>
 
-	//Define test function 1
 	complex f_analytic (math::vec3d k , math::vec3d r){
-		using math::vec3d;
 		complex f;
 		f = exp(complex(0.0,1.0)*(k | r ));
 		return f;
 	}
 
-	//Define analytic form of the gradient of the test function 1
 	complex* g_analytic (math::vec3d k , math::vec3d r) {
-		using math::vec3d;
 		static complex g[3];
 		complex factor = complex(0.0, 1.0)*exp(complex(0.0,1.0)*(k | r ));
-		for(int idir = 0; idir < 3 ; idir++) g [idir] = factor * k [idir] ;
+		for(int idir = 0; idir < 3 ; idir++) g[idir] = factor*k[idir] ;
 		return g;
 	}
 
-	//Define test function 2
 	double f_analytic2 (math::vec3d k , math::vec3d r){
 		using math::vec3d;
 		double f;
@@ -86,9 +89,7 @@ namespace operations {
 		return f;
 	}
 
-	//Define analytic form of the gradient of the test function 1
 	double* g_analytic2 (math::vec3d k , math::vec3d r) {
-		using math::vec3d;
 		static double g[3];
 		for(int idir = 0; idir < 3 ; idir++) g [idir] = k [idir] * cos (k | r);
 		return g;
@@ -116,11 +117,11 @@ TEST_CASE("function operations::gradient", "[operations::gradient]") {
 		//Define k-vector for test function
 		vec3d kvec = 2.0 * M_PI * vec3d(1.0/lx, 1.0/ly, 1.0/lz);
 
-		for(int ix = 0; ix < rs.sizes()[0]; ix++){ 			// Iterating over each x-,y- and z- components of the input field 
+		for(int ix = 0; ix < rs.sizes()[0]; ix++){
 			for(int iy = 0; iy < rs.sizes()[1]; iy++){
 				for(int iz = 0; iz < rs.sizes()[2]; iz++){
 					auto vec = rs.rvector(ix, iy, iz);
-					f_test.cubic()[ix][iy][iz] = f_analytic (kvec, vec);
+					f_test.cubic()[ix][iy][iz] = f_analytic(kvec, vec);
 				}
 			}
 		}
@@ -128,7 +129,7 @@ TEST_CASE("function operations::gradient", "[operations::gradient]") {
 		auto g_test = gradient(f_test);
 
 		double diff = 0.0;
-		for(int ix = 0; ix < rs.sizes()[0]; ix++){ 			// Iterating over each x-,y- and z- components of the input field-set 
+		for(int ix = 0; ix < rs.sizes()[0]; ix++){
 			for(int iy = 0; iy < rs.sizes()[1]; iy++){
 				for(int iz = 0; iz < rs.sizes()[2]; iz++){
 					auto vec = rs.rvector(ix, iy, iz);
@@ -136,6 +137,7 @@ TEST_CASE("function operations::gradient", "[operations::gradient]") {
 				}
 			}
 		}
+		
 		CHECK( diff < 1.0e-10 ); 
 	}
 	
@@ -143,21 +145,20 @@ TEST_CASE("function operations::gradient", "[operations::gradient]") {
 
 		basis::field<basis::real_space, double> f_test2(rs);
 	
-		//Define k-vector for test function
 		vec3d kvec = 2.0 * M_PI * vec3d(1.0/lx, 1.0/ly, 1.0/lz);
 
-		for(int ix = 0; ix < rs.sizes()[0]; ix++){ 			// Iterating over each x-,y- and z- components of the input field 
+		for(int ix = 0; ix < rs.sizes()[0]; ix++){
 			for(int iy = 0; iy < rs.sizes()[1]; iy++){
 				for(int iz = 0; iz < rs.sizes()[2]; iz++){
 					auto vec = rs.rvector(ix, iy, iz);
-					f_test2.cubic()[ix][iy][iz] = f_analytic2 (kvec, vec);
+					f_test2.cubic()[ix][iy][iz] = f_analytic2(kvec, vec);
 				}
 			}
 		}
 
 		auto g_test2 = gradient(f_test2);
 		double diff2 = 0.0;
-		for(int ix = 0; ix < rs.sizes()[0]; ix++){ 			// Iterating over each x-,y- and z- components of the input field-set 
+		for(int ix = 0; ix < rs.sizes()[0]; ix++){
 			for(int iy = 0; iy < rs.sizes()[1]; iy++){
 				for(int iz = 0; iz < rs.sizes()[2]; iz++){
 					auto vec = rs.rvector(ix, iy, iz);
@@ -165,6 +166,7 @@ TEST_CASE("function operations::gradient", "[operations::gradient]") {
 				}
 			}
 		}
+		
 		CHECK( diff2 < 1.0e-10 ); 
 	}
 }
