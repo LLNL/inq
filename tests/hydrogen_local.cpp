b46b4b16379563bea27ea6a6cd03317972398e55
Xavier Andrade <xavier@tddft.org>
2020-06-03 18:54:28 -0700

    Small increment in energy tolerance to make the test pass.

--- tests/hydrogen_local.cpp
+++ tests/hydrogen_local.cpp
@@ -31,7 +31,7 @@ int main(int argc, char ** argv){
 
 	boost::mpi3::environment env(argc, argv);
 
-	inq::utils::match energy_match(1.0e-5);
+	inq::utils::match energy_match(2.0e-5);
 
 	inq::input::species local_h = pseudo::element("H") | inq::input::species::symbol("Hloc") | inq::input::species::pseudo(inq::config::path::unit_tests_data() + "H.blyp-vbc.UPF");
 	
