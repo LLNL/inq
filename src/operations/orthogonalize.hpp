23b26fcbcfb3928d51e6a5bc0a1a5897fa710181
Xavier Andrade <xavier@tddft.org>
2020-08-27 23:44:44 -0700

    Make orthogonalize_single work in parallel.

--- src/operations/orthogonalize.hpp
+++ src/operations/orthogonalize.hpp
@@ -111,23 +111,25 @@ void orthogonalize_single(field_set_type & vec, field_set_type const & phi, int 
 		
 	for(int ist = 0; ist < num_states; ist++){
 
-
 		typename field_set_type::element_type olap = 0.0;
 		typename field_set_type::element_type norm = 0.0;
-		for(long ip = 0; ip < phi.basis().size(); ip++){
+		for(long ip = 0; ip < phi.basis().local_size(); ip++){
 			olap += conj(phi.matrix()[ip][ist])*vec.matrix()[ip][0];
 			norm += conj(phi.matrix()[ip][ist])*phi.matrix()[ip][ist];
 		}
+		
+		if(phi.basis().part().parallel()){
+			phi.basis().comm().all_reduce_in_place_n(&olap, 1, std::plus<>{});
+			phi.basis().comm().all_reduce_in_place_n(&norm, 1, std::plus<>{});
+		}
 
-		//reduce olap, norm
-
-		for(long ip = 0; ip < phi.basis().size(); ip++)	vec.matrix()[ip][0] -= olap/real(norm)*phi.matrix()[ip][ist];
+		for(long ip = 0; ip < phi.basis().local_size(); ip++)	vec.matrix()[ip][0] -= olap/real(norm)*phi.matrix()[ip][ist];
 
 #if 0
 		{
 			typename field_set_type::element_type olap = 0.0;
 				
-			for(long ip = 0; ip < phi.basis().size(); ip++){
+			for(long ip = 0; ip < phi.basis().local_size(); ip++){
 				olap += conj(phi.matrix()[ip][ist])*vec.matrix()[ip][0];
 			}
 				
