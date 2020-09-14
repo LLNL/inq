e513156890e50b6208ea7d434b5cde0c6d0dc1a7
Alfredo Correa <alfredo.correa@gmail.com>
2020-09-13 23:01:37 -0700

    simplified cmake_intel

--- ci/cmake_intel.sh
+++ ci/cmake_intel.sh
@@ -4,8 +4,7 @@ mkdir ./intel.build
 mkdir ./intel.install
 cd intel.build
 rm -f CMakeCache.txt
-CC=/opt/intel/system_studio_2020/bin/icc CXX=/opt/intel/system_studio_2020/bin/icpc \
-  cmake ../.. \
+cmake ../.. \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_C_COMPILER=/opt/intel/system_studio_2020/bin/icc \
     -DCMAKE_CXX_COMPILER=/opt/intel/system_studio_2020/bin/icpc \
@@ -13,9 +12,10 @@ CC=/opt/intel/system_studio_2020/bin/icc CXX=/opt/intel/system_studio_2020/bin/i
     -DCMAKE_INSTALL_PREFIX=`pwd`/../intel.install \
     -DCMAKE_C_COMPILE_FLAGS="$(mpicxx -showme:compile || mpicxx -cxx= -compile_info)" \
     -DCMAKE_CXX_COMPILE_FLAGS="$(mpicxx -showme:compile || mpicxx -cxx= -compile_info)" \
-    -DCMAKE_CXX_FLAGS="-O3 -Wall -Wextra -Wl,-rpath,/opt/intel/system_studio_2020/lib/intel64" \
+    -DCMAKE_CXX_FLAGS="-O3 -Wall -Wextra -Wl,-rpath,/opt/intel/system_studio_2020/lib/intel64 $(mpicxx -showme:compile || mpicxx -cxx= -compile_info)" \
     -DCMAKE_EXE_LINKER_FLAGS="$(mpicxx -showme:link || mpicxx -cxx= -link_info)" \
-    $* && \
-    make -j $(($(nproc)/2 + 1)) && \
-    make install || exit 1
+    $*  \
+  && make -j $(($(nproc)/2 + 1)) \
+  && make install \
+  && ctest -j $(nproc) --output-on-failure || exit 1
 
