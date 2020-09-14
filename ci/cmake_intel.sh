b8f80b31eafe3be8e1f6c6522dd3e16277b7ed7b
Alfredo Correa <alfredo.correa@gmail.com>
2020-09-13 22:39:57 -0700

    restore original CMakeList without conditional

--- ci/cmake_intel.sh
+++ ci/cmake_intel.sh
@@ -1,20 +1,21 @@
-. /opt/intel/system_studio_2020/bin/compilervars.sh intel64
-rm -rf ./intel.dir.build ./intel.dir.install 
-mkdir ./intel.dir.build
-mkdir ./intel.dir.install
-cd intel.dir.build
+#source /opt/intel/system_studio_2020/bin/compilervars.sh intel64
+rm -rf ./intel.build ./intel.install 
+mkdir ./intel.build
+mkdir ./intel.install
+cd intel.build
 rm -f CMakeCache.txt
-cmake ../.. \
-	-DCMAKE_BUILD_TYPE=Debug \
-	-DCMAKE_C_COMPILER=/opt/intel/system_studio_2020/bin/icc \
-	-DCMAKE_CXX_COMPILER=/opt/intel/system_studio_2020/bin/icpc \
-	-DCMAKE_LINKER=/opt/intel/system_studio_2020/bin/icpc \
-	-DCMAKE_INSTALL_PREFIX=`pwd`/../intel.dir.install \
-	-DCMAKE_C_COMPILE_FLAGS="$(mpicxx -showme:compile || mpicxx -cxx= -compile_info)" \
-	-DCMAKE_CXX_COMPILE_FLAGS="$(mpicxx -showme:compile || mpicxx -cxx= -compile_info)" \
-	-DCMAKE_CXX_FLAGS="-I/usr/lib/x86_64-linux-gnu/openmpi/include/openmpi -I/usr/lib/x86_64-linux-gnu/openmpi/include -pthread -O3 -Wall -Wextra -Warray-bounds -Wcomment -Wenum-compare -Wformat -Wuninitialized -Wmaybe-uninitialized -Wmain -Wnarrowing -Wnonnull -Wparentheses -Wreorder -Wreturn-type -Wsign-compare -Wsequence-point -Wtrigraphs -Wunused-function -Wunused-but-set-variable -Wunused-variable -Wwrite-strings -Wno-unused-parameter -Werror -Wl,-rpath,/opt/intel/system_studio_2020/lib/intel64" \
-	-DCMAKE_EXE_LINKER_FLAGS="$(mpicxx -showme:link || mpicxx -cxx= -link_info)" \
-	$* && \
-make -j $(($(nproc)/2 + 1)) && \
-make install || exit 1
+CC=/opt/intel/system_studio_2020/bin/icc CXX=/opt/intel/system_studio_2020/bin/icpc \
+  cmake ../.. \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_C_COMPILER=/opt/intel/system_studio_2020/bin/icc \
+    -DCMAKE_CXX_COMPILER=/opt/intel/system_studio_2020/bin/icpc \
+    -DCMAKE_LINKER=/opt/intel/system_studio_2020/bin/icpc \
+    -DCMAKE_INSTALL_PREFIX=`pwd`/../intel.install \
+    -DCMAKE_C_COMPILE_FLAGS="$(mpicxx -showme:compile || mpicxx -cxx= -compile_info)" \
+    -DCMAKE_CXX_COMPILE_FLAGS="$(mpicxx -showme:compile || mpicxx -cxx= -compile_info)" \
+    -DCMAKE_CXX_FLAGS="-O3 -Wall -Wextra -Wl,-rpath,/opt/intel/system_studio_2020/lib/intel64" \
+    -DCMAKE_EXE_LINKER_FLAGS="$(mpicxx -showme:link || mpicxx -cxx= -link_info)" \
+    $* && \
+    make -j $(($(nproc)/2 + 1)) && \
+    make install || exit 1
 
