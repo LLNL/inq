09f51c57c6b6e7dfe07d18bc175c4e49098b482f
Xavier Andrade <xavier@tddft.org>
2020-09-19 04:02:00 -0700

    Make eigenvalue a scalar.

--- src/eigensolvers/conjugate_gradient.hpp
+++ src/eigensolvers/conjugate_gradient.hpp
@@ -51,8 +51,8 @@ void conjugate_gradient(const operator_type & ham, const preconditioner_type & p
       
 		auto hphi = ham(phi);
 
-		auto eigenvalue = operations::overlap_diagonal(hphi, phi);
-		auto old_energy = eigenvalue[0];
+		auto eigenvalue = operations::overlap_diagonal(hphi, phi)[0];
+		auto old_energy = eigenvalue;
 
 		double first_delta_e = 0.0;
 			
@@ -62,11 +62,11 @@ void conjugate_gradient(const operator_type & ham, const preconditioner_type & p
       
 		for(int iter = 0; iter < num_iter + 1; iter++){
 
-			eigenvalue = operations::overlap_diagonal(phi, hphi);
+			eigenvalue = operations::overlap_diagonal(phi, hphi)[0];
         
 			basis::field_set<basis::fourier_space, field_set_type> g(phi_all.basis(), 1, phi_all.full_comm());
 
-			for(long ip = 0; ip < g.basis().local_size(); ip++) g.matrix()[ip][0] = hphi.matrix()[ip][0] - eigenvalue[0]*phi.matrix()[ip][0];
+			for(long ip = 0; ip < g.basis().local_size(); ip++) g.matrix()[ip][0] = hphi.matrix()[ip][0] - eigenvalue*phi.matrix()[ip][0];
 
 			double res = fabs(operations::overlap_diagonal(g)[0]);
 
@@ -132,7 +132,7 @@ void conjugate_gradient(const operator_type & ham, const preconditioner_type & p
 
 			a0 = 2.0*a0/cg0;
 			b0 = b0/(cg0*cg0);
-			auto alpha = 2.0*real(eigenvalue[0] - b0);
+			auto alpha = 2.0*real(eigenvalue - b0);
 			auto beta = real(a0)*2.0;
 
 			auto theta = atan(beta/alpha)*0.5;
@@ -170,21 +170,21 @@ void conjugate_gradient(const operator_type & ham, const preconditioner_type & p
 			*/
 			
 			//calculate the eigenvalue, this is duplicated
-			eigenvalue = operations::overlap_diagonal(phi, hphi);
+			eigenvalue = operations::overlap_diagonal(phi, hphi)[0];
         
 			basis::field_set<basis::fourier_space, field_set_type> g2(phi_all.basis(), 1, phi_all.full_comm());
 				
-			for(long ip = 0; ip < g.basis().local_size(); ip++) g2.matrix()[ip][0] = hphi.matrix()[ip][0] - eigenvalue[0]*phi.matrix()[ip][0];
+			for(long ip = 0; ip < g.basis().local_size(); ip++) g2.matrix()[ip][0] = hphi.matrix()[ip][0] - eigenvalue*phi.matrix()[ip][0];
 				
 			res = fabs(operations::overlap_diagonal(g2)[0]);
 				
 			if(iter > 0){
-				if(fabs(eigenvalue[0] - old_energy) < first_delta_e*energy_change_threshold) {
+				if(fabs(eigenvalue - old_energy) < first_delta_e*energy_change_threshold) {
 					//						std::cout << "energy_change_threshold " << iter << std::endl;
 					break;
 				}
 			}	else {
-				first_delta_e = fabs(eigenvalue[0] - old_energy);
+				first_delta_e = fabs(eigenvalue - old_energy);
 				if(first_delta_e <= 2.0*std::numeric_limits<decltype(first_delta_e)>::epsilon()) {
 					//						std::cout << "zero first_delta_e" << std::endl;
 					break;
@@ -196,7 +196,7 @@ void conjugate_gradient(const operator_type & ham, const preconditioner_type & p
 				break;
 			}
 			
-			old_energy = eigenvalue[0];
+			old_energy = eigenvalue;
 				
 		} // end iteration
 
