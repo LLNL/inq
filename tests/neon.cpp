1da0b49485d2c4c04b9ec6fc1181dff8bdc59432
Xavier Andrade <xavier@tddft.org>
2020-11-03 10:48:43 -0800

    Updated the neon test results due to the fix in the preconditioner.

--- tests/neon.cpp
+++ tests/neon.cpp
@@ -87,12 +87,12 @@ int main(int argc, char ** argv){
 
 		*/
 		
-		energy_match.check("total energy",     result.energy.total()    , -66.260996130282);
-		energy_match.check("kinetic energy",   result.energy.kinetic()  ,  35.546509585678);
-		energy_match.check("eigenvalues",      result.energy.eigenvalues, -61.740955117197);
-		energy_match.check("external energy",  result.energy.external   , -79.409327530205);
-		energy_match.check("non-local energy", result.energy.nonlocal   , -17.878137172671);
-		energy_match.check("ion-ion energy",   result.energy.ion        , -4.52004101);
+		energy_match.check("total energy",     result.energy.total()    , -66.260996131073);
+		energy_match.check("kinetic energy",   result.energy.kinetic()  ,  35.546488765843);
+		energy_match.check("eigenvalues",      result.energy.eigenvalues, -61.740955117988);
+		energy_match.check("external energy",  result.energy.external   , -79.409315995073);
+		energy_match.check("non-local energy", result.energy.nonlocal   , -17.878127888758);
+		energy_match.check("ion-ion energy",   result.energy.ion        , -4.520041013085);
 		
 	}
 
@@ -141,11 +141,11 @@ int main(int argc, char ** argv){
 
 		*/
 		
-		energy_match.check("total energy",     result.energy.total()    , -66.260998365817);
-		energy_match.check("kinetic energy",   result.energy.kinetic()  ,  35.546492603119);
-		energy_match.check("eigenvalues",      result.energy.eigenvalues, -61.740957352732);
-		energy_match.check("external energy",  result.energy.external   , -79.409319159042);
-		energy_match.check("non-local energy", result.energy.nonlocal   , -17.878130796809);
+		energy_match.check("total energy",     result.energy.total()    , -66.260998366598);
+		energy_match.check("kinetic energy",   result.energy.kinetic()  ,  35.546473395356);
+		energy_match.check("eigenvalues",      result.energy.eigenvalues, -61.740957353513);
+		energy_match.check("external energy",  result.energy.external   , -79.409308401592);
+		energy_match.check("non-local energy", result.energy.nonlocal   , -17.878122347276);
 		energy_match.check("ion-ion energy",   result.energy.ion        ,  -4.520041013085);
 		
 	}
