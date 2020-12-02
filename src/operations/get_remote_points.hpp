efbd216a3d77cffeaad2680787b2a8c43c409464
Xavier Andrade <xavier@tddft.org>
2020-12-02 05:19:02 -0800

    Fixed copyright year.

--- src/operations/get_remote_points.hpp
+++ src/operations/get_remote_points.hpp
@@ -4,7 +4,7 @@
 #define INQ__UTILS__GET_REMOTE_POINTS
 
 /*
- Copyright (C) 2019 Xavier Andrade
+ Copyright (C) 2020 Xavier Andrade
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by
