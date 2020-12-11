ca012253f36af458c6421dfeac1e0e92cd8d3290
Alfredo Correa <alfredo.correa@gmail.com>
2020-12-11 10:24:35 -0800

    use namespace for gemm

--- src/ground_state/subspace_diagonalization.hpp
+++ src/ground_state/subspace_diagonalization.hpp
@@ -49,7 +49,8 @@ auto subspace_diagonalization(const hamiltonian_type & ham, field_set_type & phi
 	using boost::multi::blas::gemm;
 	using boost::multi::blas::hermitized;
 	
-	phi.matrix() = gemm(phi.matrix(), hermitized(subspace_hamiltonian));
+	namespace blas = boost::multi::blas;
+	phi.matrix() = blas::gemm(1., phi.matrix(), blas::H(subspace_hamiltonian));
 
 	return eigenvalues;	
 }
