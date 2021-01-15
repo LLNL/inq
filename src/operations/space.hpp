a591cc34e9d86ce08aea86aa5d273a3b1cc9867e
Alfredo Correa <alfredo.correa@gmail.com>
2021-01-14 17:56:04 -0800

    make assigments with thrust-based copy

--- src/operations/space.hpp
+++ src/operations/space.hpp
@@ -117,23 +117,23 @@ void to_fourier(basis::real_space const & real_basis, basis::fourier_space const
 		}
 		
 		// we should do
-		//   math::array<complex, 5> buffer = tmp.unrotated(2).partitioned(comm.size()).transposed().rotated().transposed().rotated();
+		math::array<complex, 5> buffer = tmp.unrotated(2).partitioned(comm.size()).transposed().rotated().transposed().rotated();
 		// but it is impossibly slow
 		// so for the moment we do:
 		
-		math::array<complex, 5> buffer({comm.size(), xblock, real_basis.local_sizes()[1], zblock, last_dim});
+//		math::array<complex, 5> buffer({comm.size(), xblock, real_basis.local_sizes()[1], zblock, last_dim});
 
-		for(int i4 = 0; i4 < comm.size(); i4++){
-			CALI_CXX_MARK_SCOPE("fft_transpose");
-			
-			gpu::run(last_dim, zblock, real_basis.local_sizes()[1], xblock, 
-							 [i4,
-								buf = begin(buffer),
-								rot = begin(tmp.unrotated(2).partitioned(comm.size()).transposed().rotated().transposed().rotated())]
-							 GPU_LAMBDA (auto i0, auto i1, auto i2, auto i3){
-								 buf[i4][i3][i2][i1][i0] = rot[i4][i3][i2][i1][i0];
-							 });
-		}
+//		for(int i4 = 0; i4 < comm.size(); i4++){
+//			CALI_CXX_MARK_SCOPE("fft_transpose");
+//			
+//			gpu::run(last_dim, zblock, real_basis.local_sizes()[1], xblock, 
+//							 [i4,
+//								buf = begin(buffer),
+//								rot = begin(tmp.unrotated(2).partitioned(comm.size()).transposed().rotated().transposed().rotated())]
+//							 GPU_LAMBDA (auto i0, auto i1, auto i2, auto i3){
+//								 buf[i4][i3][i2][i1][i0] = rot[i4][i3][i2][i1][i0];
+//							 });
+//		}
 
 		assert(std::get<4>(sizes(buffer)) == last_dim);
 		
