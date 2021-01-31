1d8b7b4416fbc7d2ecbdfdda18cc5bfc6a7379e5
Xavier Andrade <xavier@tddft.org>
2021-01-30 22:34:52 -0800

    Allow an alternative path for FFTW.

--- .configure-custom.sh
+++ .configure-custom.sh
@@ -1,5 +1,5 @@
 ENABLE_VARS="cuda|ON|ENABLE_CUDA heffte|ON|ENABLE_HEFFTE"
-WITH_VARS="cuda-prefix"
+WITH_VARS="cuda-prefix fftw-prefix|yes|FFTW_ROOT"
 
 ENABLE_CUDA_DOC="Compile with GPU support through Nvidia Cuda"
 WITH_CUDA_PREFIX_DOC="Location of the cuda installation (currently ignored)"
