e21e11a2048ab976b2509d4c7d6f051e2d0c166d
Alfredo Correa <alfredo.correa@gmail.com>
2021-02-24 17:26:41 -0800

    use cbase for hint

--- src/basis/spherical_grid.hpp
+++ src/basis/spherical_grid.hpp
@@ -151,7 +151,7 @@ namespace basis {
 			const int nst = std::get<3>(sizes(grid));
 
 			CALI_MARK_BEGIN("spherical_grid::gather(4d)::allocation");
-			math::array<typename array_4d::element, 2> subgrid({this->size(), nst});
+			math::array<typename array_4d::element, 2> subgrid({this->size(), nst}, grid.cbase());
 			CALI_MARK_END("spherical_grid::gather(4d)::allocation");
 
 			math::prefetch(subgrid);
