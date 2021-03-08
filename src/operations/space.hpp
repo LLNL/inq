6cc42cfa47055e104252a6dfb83912eb9046eb44
Alfredo Correa <alfredo.correa@gmail.com>
2021-03-08 02:36:33 -0800

    update credits

--- src/operations/space.hpp
+++ src/operations/space.hpp
@@ -4,7 +4,7 @@
 #define INQ__OPERATIONS__SPACE
 
 /*
- Copyright (C) 2019 Xavier Andrade, Alfredo A. Correa.
+ Copyright (C) 2019-2021 Xavier Andrade, Alfredo A. Correa.
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by
