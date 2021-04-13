0d18f73aa4a047b2cec7ae4ac11a7f470d8cb9fa
Alfredo Correa <alfredo.correa@gmail.com>
2021-04-13 02:34:49 -0700

    update credits

--- src/operations/overlap_diagonal.hpp
+++ src/operations/overlap_diagonal.hpp
@@ -4,7 +4,7 @@
 #define OPERATIONS__OVERLAP_DIAGONAL
 
 /*
- Copyright (C) 2019-2021 Xavier Andrade, Alfredo A. Correa.
+ Copyright (C) 2019-2020 Xavier Andrade, Alfredo A. Correa.
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by
