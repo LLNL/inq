603680a29e7ee861204f0085e9292e9efc91da79
Alfredo Correa <alfredo.correa+work@gmail.com>
2021-05-21 22:29:05 +0000

    use paren for initialize vector3 from scalar

--- src/ground_state/calculate.hpp
+++ src/ground_state/calculate.hpp
@@ -228,7 +228,7 @@ namespace ground_state {
 		if(ions.cell().periodic_dimensions() == 0){
 			res.dipole = observables::dipole(ions, electrons);
 		} else {
-			res.dipole = 0.0;
+			res.dipole(0.0);
 		}
 
 
