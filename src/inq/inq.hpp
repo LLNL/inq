a59167ed41d29bf9821940d0f0a56d731b5beecd
Xavier Andrade <xavier@tddft.org>
2021-05-31 22:30:38 -0700

    Added some files to the main inq include.

--- src/inq/inq.hpp
+++ src/inq/inq.hpp
@@ -18,6 +18,8 @@
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+#include <utils/match.hpp>
+#include <ground_state/initialize.hpp>
 #include <ground_state/calculate.hpp>
 #include <real_time/propagate.hpp>
 
