46dd1cb402715a952366794f8cc6eaaf8701f236
Xavier Andrade <xavier@tddft.org>
2021-07-07 13:28:22 -0700

    Proof of concept call to spglib.

--- src/systems/ions.hpp
+++ src/systems/ions.hpp
@@ -5,6 +5,8 @@
 #ifndef INQ__SYSTEMS__IONS
 #define INQ__SYSTEMS__IONS
 
+#include <spglib.h>
+
 #include <ions/geometry.hpp>
 #include <ions/unitcell.hpp>
 #include <input/cell.hpp>
@@ -21,7 +23,20 @@ public:
 	ions(const input::cell & arg_cell_input, const inq::ions::geometry & geo_arg = inq::ions::geometry()):
 		cell_(arg_cell_input, arg_cell_input.periodic_dimensions()),
 		geo_(geo_arg)
-	{}
+	{
+		char symbol[11];
+
+		std::vector<int> types(geo_.num_atoms());
+
+		for(int iatom = 0; iatom < geo_.num_atoms(); iatom++){
+			types[iatom] = geo_.atoms()[iatom].atomic_number();
+		}
+		
+		auto symnum = spg_get_international(symbol, reinterpret_cast<double (*)[3] >(const_cast<double *>(cell_.amat())), reinterpret_cast<double (*)[3] >(const_cast<math::vector3<double> *>(geo_.coordinates().data())),
+																				types.data(), geo_.num_atoms(), 1e-4);
+		
+		std::cout << "symmetry: " << symbol << '\t' << symnum << std::endl;
+	}
 
 	auto & geo() const {
 		return geo_;
