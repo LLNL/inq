4ab80261756efe291bcf4a16a790222f470a00ae
Xavier Andrade <xavier@tddft.org>
2022-01-12 17:05:33 -0300

    Added a distribution class that will contain a communicator and information how to distribute the data in parallel.

---
+++ src/input/distribution.hpp
@@ -0,0 +1,57 @@
+/* -*- indent-tabs-mode: t -*- */
+
+#ifndef INQ__INPUT__DISTRIBUTION
+#define INQ__INPUT__DISTRIBUTION
+
+/*
+ Copyright (C) 2020 Xavier Andrade
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
+  
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+  
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <mpi3/communicator.hpp>
+
+namespace inq {
+namespace input {
+
+  class distribution {
+
+  public:
+
+    distribution(boost::mpi3::communicator & comm):
+      comm_(comm){
+    }
+    
+    auto & comm() {
+      return comm_;
+    }
+    
+	private:
+		
+    mutable boost::mpi3::communicator comm_;
+    
+  };
+    
+}
+}
+
+////////////////////////////////////////////////////////
+
+#ifdef INQ_INPUT_DISTRIBUTION_UNIT_TEST
+#undef INQ_INPUT_DISTRIBUTION_UNIT_TEST
+
+#endif
+   
+#endif
