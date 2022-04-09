c7f28b2bbaaffec6071735c1290bb0b0e6ec8c93
Xavier Andrade <xavier@tddft.org>
2022-04-08 21:45:56 -0700

    Added a file and test for the steepest_descent linear solver, that still needs to be implemented.

---
+++ src/solvers/steepest_descent.hpp
@@ -0,0 +1,142 @@
+/* -*- indent-tabs-mode: t -*- */
+
+#ifndef INQ__SOLVERS__STEEPEST_DESCENT
+#define INQ__SOLVERS__STEEPEST_DESCENT
+
+/*
+ Copyright (C) 2019 Xavier Andrade
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
+  
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+  
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <math/complex.hpp>
+#include <math/vector3.hpp>
+#include <math/array.hpp>
+#include <hamiltonian/ks_hamiltonian.hpp>
+#include <operations/shift.hpp>
+#include <operations/orthogonalize.hpp>
+#include <operations/overlap_diagonal.hpp>
+
+namespace inq {
+namespace solvers {
+
+template <class operator_type, class preconditioner_type, class field_set_type>
+void steepest_descent(const operator_type & ham, const preconditioner_type & prec, field_set_type & rhs, field_set_type & phi){
+	CALI_CXX_MARK_SCOPE("solver::steepest_descent");
+
+}
+
+}
+}
+
+#ifdef INQ_SOLVERS_STEEPEST_DESCENT_UNIT_TEST
+#undef INQ_SOLVERS_STEEPEST_DESCENT_UNIT_TEST
+
+#include <basis/trivial.hpp>
+#include <ions/unitcell.hpp>
+#include <operations/matrix_operator.hpp>
+
+#include <catch2/catch_all.hpp>
+
+TEST_CASE("solvers::steepest_descent", "[solvers::steepest_descent]") {
+
+	using namespace inq;
+	using namespace Catch::literals;
+	
+  const int npoint = 100;
+  const int nvec = 12;
+  
+  basis::trivial bas(npoint);
+	
+	math::array<complex, 2> identity_matrix({npoint, npoint});
+  
+	for(int ip = 0; ip < npoint; ip++){
+		for(int jp = 0; jp < npoint; jp++){
+			identity_matrix[ip][jp] = 0.0;
+			if(ip == jp) identity_matrix[ip][jp] = 1.0;
+		}
+	}
+  
+	operations::matrix_operator<complex> identity(std::move(identity_matrix));
+
+	SECTION("Diagonal matrix complex"){
+  
+    math::array<complex, 2> diagonal_matrix({npoint, npoint});
+    
+    for(int ip = 0; ip < npoint; ip++){
+      for(int jp = 0; jp < npoint; jp++){
+        diagonal_matrix[ip][jp] = 0.0;
+        if(ip == jp) diagonal_matrix[ip][jp] = ip + 1.0;
+      }
+    }
+    
+    operations::matrix_operator<complex> diagonal_op(std::move(diagonal_matrix));
+    
+    basis::field_set<basis::trivial, complex> phi(bas, nvec);
+    basis::field_set<basis::trivial, complex> rhs(bas, nvec);
+ 
+		for(int ip = 0; ip < npoint; ip++){
+      for(int ivec = 0; ivec < nvec; ivec++){
+        phi.matrix()[ip][ivec] = exp(complex(0.0, (ip*ivec)*0.1));
+        rhs.matrix()[ip][ivec] = ivec + 1.0;
+      }
+    }
+
+		const int num_iter = 100;
+		
+		for(int iter = 0; iter < num_iter; iter++){
+
+			solvers::steepest_descent(diagonal_op, identity, rhs, phi);
+			
+			auto residual = diagonal_op(phi);
+			operations::shift(-1.0, rhs, residual);
+			auto normres = operations::overlap_diagonal(residual);
+			
+      
+      tfm::format(std::cout, "  Iteration %4d:\n", iter);
+			
+      for(int ivec = 0; ivec < phi.set_size(); ivec++){
+				tfm::format(std::cout, "    state %4d  res = %15.10e\n", ivec + 1, real(normres[ivec]));
+      }
+      /*
+			if(num_iter - 1 == iter){
+
+				CHECK(fabs(eigenvalues[0]) == 1.000000001634_a);
+				CHECK(fabs(eigenvalues[1]) == 2.000000003689_a);
+				CHECK(fabs(eigenvalues[2]) == 3.000000001955_a);
+				CHECK(fabs(eigenvalues[3]) == 4.000000001760_a);
+				CHECK(fabs(eigenvalues[4]) == 5.000000002561_a);
+				CHECK(fabs(eigenvalues[5]) == 6.000000003127_a);
+				CHECK(fabs(eigenvalues[6]) == 7.000000002312_a);
+				CHECK(fabs(eigenvalues[7]) == 8.000000000292_a);
+				CHECK(fabs(eigenvalues[8]) == 8.999999999033_a);
+				CHECK(fabs(eigenvalues[9]) == 9.999999998497_a);
+				CHECK(fabs(eigenvalues[10]) == 10.999999998768_a);
+				CHECK(fabs(eigenvalues[11]) == 11.999999998422_a);
+
+			}
+			*/
+		}
+ 	
+ }
+
+
+}
+
+
+#endif
+
+
+#endif
