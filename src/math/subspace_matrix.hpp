b62475f9103ae8e46b3514db3aa1c8b3e93f4ffb
Xavier Andrade <xavier@tddft.org>
2022-05-24 16:42:50 -0700

    Added a class for the matrices in the subspace of KS states.

---
+++ src/math/subspace_matrix.hpp
@@ -0,0 +1,84 @@
+/* -*- indent-tabs-mode: t -*- */
+
+#ifndef INQ__MATH__SUBSPACE_MATRIX
+#define INQ__MATH__SUBSPACE_MATRIX
+
+/*
+ Copyright (C) 2019-2021 Xavier Andrade, Alfredo A. Correa.
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
+  
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+  
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <math/array.hpp>
+
+namespace inq {
+namespace math {
+
+template <class Type>
+class subspace_matrix {
+
+public:
+  
+  using array_type = math::array<Type, 2>;
+  
+  subspace_matrix(array_type && mat):
+    array_(std::move(mat)){
+  }
+  
+  auto & array() const {
+    return array_;
+  }
+
+  auto & array() {
+    return array_;
+  }
+  
+private:
+
+  array_type array_;
+  
+};
+
+}
+}
+
+#ifdef INQ_MATH_SUBSPACE_MATRIX_UNIT_TEST
+#undef INQ_MATH_SUBSPACE_MATRIX_UNIT_TEST
+
+#include <catch2/catch_all.hpp>
+
+TEST_CASE("math::subspace_matrix", "[math::subspace_matrix]") {
+	using namespace inq;
+	using namespace Catch::literals;
+	using Catch::Approx;
+
+  math::array<double, 2> matrix({2, 2});
+  
+  matrix[0][0] = 4.0;
+  matrix[0][1] = 0.0;
+  matrix[1][0] = 0.0;
+  matrix[1][1] = 2.0;
+
+  math::subspace_matrix mm(std::move(matrix));
+
+  CHECK(mm.array()[0][0] == 4.0);
+  CHECK(mm.array()[0][1] == 0.0);
+  CHECK(mm.array()[1][0] == 0.0);
+  CHECK(mm.array()[1][1] == 2.0);
+  
+}
+
+#endif
+#endif
