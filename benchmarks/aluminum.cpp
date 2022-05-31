e1660fd7e70b5578e38da1967df0fdd951e1f68a
Xavier Andrade <xavier@tddft.org>
2022-05-30 23:57:47 -0700

    Store the reps in an array.

--- benchmarks/aluminum.cpp
+++ benchmarks/aluminum.cpp
@@ -31,11 +31,8 @@ int main(int argc, char ** argv){
 
 	int pardomains = 1;
 	bool groundstate_only = false;
-	int repx = 2;
-	int repy = 2;
-	int repz = 2;
-
-
+	math::vector3 reps{2, 2, 2};
+	
 	{
 		int opt;
 		while ((opt = getopt(argc, argv, "p:?gs:")) != EOF){
@@ -49,7 +46,7 @@ int main(int argc, char ** argv){
 			case 's':
 				{
 					std::stringstream instr(optarg);
-					instr >> repx >> repy >> repz;
+					instr >> reps;
 				}
 				break;
 			case '?':
@@ -64,8 +61,6 @@ int main(int argc, char ** argv){
 		}
 	}
 	
-	utils::match energy_match(4.0e-6);
-
 	auto alat = 7.6524459_bohr;
 
 	using frac_coord = math::vector3<decltype(0.0_crys)>;
@@ -76,16 +71,14 @@ int main(int argc, char ** argv){
 	cell.emplace_back(frac_coord{0.0_crys, 0.5_crys, 0.5_crys});
 	cell.emplace_back(frac_coord{0.5_crys, 0.0_crys, 0.5_crys});
 	cell.emplace_back(frac_coord{0.5_crys, 0.5_crys, 0.0_crys});
-
-	auto reps = math::vector3{repx, repy, repz};
 	
-	systems::box box = systems::box::orthorhombic(repx*alat, repy*alat, repz*alat).spacing(alat/20);
+	systems::box box = systems::box::orthorhombic(reps[0]*alat, reps[1]*alat, reps[2]*alat).spacing(alat/20);
 	
 	systems::ions ions(box);
 	
-	for(int ix = 0; ix < repx; ix++){
-		for(int iy = 0; iy < repy; iy++){
-			for(int iz = 0; iz < repz; iz++){
+	for(int ix = 0; ix < reps[0]; ix++){
+		for(int iy = 0; iy < reps[1]; iy++){
+			for(int iz = 0; iz < reps[2]; iz++){
 				frac_coord base{ix*1.0_crys, iy*1.0_crys, iz*1.0_crys};
 				for(unsigned iatom = 0; iatom < cell.size(); iatom++){
 					ions.insert("Al", (base + cell[iatom])/reps);
@@ -93,17 +86,17 @@ int main(int argc, char ** argv){
 			}
 		}
 	}
-
-	assert(int(ions.geo().num_atoms()) == int(cell.size()*repx*repy*repz));
-
+	
+	assert(int(ions.geo().num_atoms()) == int(cell.size()*product(reps)));
+				 
 	input::config conf;
-
-	conf.extra_states = 2*repx*repy*repz;
+				 
+	conf.extra_states = 2*product(reps);
 	conf.temperature = 300.0_K;	
 	
 	systems::electrons electrons(env.par().states().domains(pardomains), ions, box, conf);
 	
-	auto restart_dir = "aluminum_" + std::to_string(repx) + "_" + std::to_string(repy) + "_" + std::to_string(repz);
+	auto restart_dir = "aluminum_" + std::to_string(reps[0]) + "_" + std::to_string(reps[1]) + "_" + std::to_string(reps[2]);
 
 	auto not_found_gs = groundstate_only or not electrons.load(restart_dir);
 
@@ -116,8 +109,6 @@ int main(int argc, char ** argv){
 	if(not groundstate_only){
 		auto propagation = real_time::propagate(ions, electrons, input::interaction::pbe(), input::rt::num_steps(100) | input::rt::dt(0.0565_atomictime));
 	}
-	
-	return energy_match.fail();
 	
 }
 
