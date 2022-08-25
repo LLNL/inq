7b2e89164a6ca8d8b62a6f632802282128157abd
Alfredo Correa <correaa@llnl.gov>
2022-08-24 18:30:59 -0700

    update credits

--- src/solvers/steepest_descent.hpp
+++ src/solvers/steepest_descent.hpp
@@ -4,7 +4,7 @@
 #define INQ__SOLVERS__STEEPEST_DESCENT
 
 /*
- Copyright (C) 2019 Xavier Andrade
+ Copyright (C) 2019-2022 Xavier Andrade, Alfredo A. Correa
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by
@@ -92,9 +92,9 @@ double steepest_descent(const operator_type & ham, const preconditioner_type & p
 
 	auto maxloc =
 #ifdef HAVE_CUDA
-		thrust::max_element(normres.begin(), normres.end());
+		thrust::max_element(normres.begin(), normres.end())
 #else
-		std   ::max_element(normres.begin(), normres.end());
+		std   ::max_element(normres.begin(), normres.end())
 #endif
 	;
 	if(maxloc == normres.end()) return 0.0;
