4b323769ee0b4e023fa2b58044bf56099033907d
Xavier Andrade <xavier@tddft.org>
2022-10-18 00:31:54 -0700

    Added a test for a vector3 spectrum.

--- src/observables/spectrum.hpp
+++ src/observables/spectrum.hpp
@@ -49,7 +49,7 @@ math::array<RetElementType, 1> spectrum(quantity<magnitude::energy> maxw, quanti
              
              double ww = dw.in_atomic_units()*ifreq;
 
-             RetElementType sum = 0.5*(tim[1] - tim[0])*tse[0];
+             RetElementType sum = 0.5*(tim[1] - tim[0])*tse[0]*exp(complex{0.0, 1.0}*0.0);
              for(long itime = 1; itime < ntime - 1; itime++){
                assert(tim[itime] > tim[itime - 1]);
 
@@ -95,6 +95,8 @@ TEST_CASE("observables::spectrum", "[observables::spectrum]") {
 
   math::array<double, 1> time(ntime);
   math::array<double, 1> tseries(ntime);
+  math::array<math::vector3<double>, 1> vtseries(ntime);
+  
   double freq1 = 10.0;
   double freq2 = 6.39;
   double amp1 = 2.0;
@@ -102,22 +104,43 @@ TEST_CASE("observables::spectrum", "[observables::spectrum]") {
   
   for(int itime = 0; itime < ntime; itime++){
     time[itime] = dtime*itime;
-    tseries[itime] = amp1*cos(time[itime]*freq1) + amp2*sin(time[itime]*freq2);
+    auto func = amp1*cos(time[itime]*freq1) + amp2*sin(time[itime]*freq2);
+    tseries[itime] = func;
+    vtseries[itime] = math::vector3<double>{-1.0*func, 20.0*func, -0.3*func};
   }
 
   auto maxw = 20.0_Ha;
   auto dw = 0.1_Ha;
   
   auto fseries = observables::spectrum(maxw, dw, time, tseries);
+  auto vfseries = observables::spectrum(maxw, dw, time, vtseries);
 
   auto nfreq = fseries.size();
 
   CHECK(nfreq == 201);
 
+  CHECK(real(fseries[12]) == -0.2352749195_a);
+  CHECK(imag(fseries[12]) == -0.0264556811_a);
+  CHECK(real(vfseries[12][0]) == 0.2352749195_a);
+  CHECK(imag(vfseries[12][0]) == 0.0264556811_a);
+  CHECK(real(vfseries[12][1]) == -4.7054983903_a);
+  CHECK(imag(vfseries[12][1]) == -0.5291136216_a);
+  CHECK(real(vfseries[12][2]) == 0.0705824759_a);
+  CHECK(imag(vfseries[12][2]) == 0.0079367043_a);
+  
+  CHECK(real(fseries[100]) == 50.1204711636_a);
+  CHECK(imag(fseries[100]) == 0.0321104817_a);
+  CHECK(real(vfseries[100][0]) == -50.1204711636_a);
+  CHECK(imag(vfseries[100][0]) == -0.0321104817_a);
+  CHECK(real(vfseries[100][1]) == 1002.4094232726_a);
+  CHECK(imag(vfseries[100][1]) == 0.6422096339_a);
+  CHECK(real(vfseries[100][2]) == -15.0361413491_a);
+  CHECK(imag(vfseries[100][2]) == -0.0096331445_a);
+
   std::ofstream file("spectrum.dat");
   
   for(int ifreq = 0; ifreq < nfreq; ifreq++){
-    file << ifreq*dw.in_atomic_units() << '\t' << real(fseries[ifreq]) << '\t' << imag(fseries[ifreq]) << std::endl;
+    file << ifreq*dw.in_atomic_units() << '\t' << real(fseries[ifreq]) << '\t' << imag(fseries[ifreq]) << '\t' << real(vfseries[ifreq]) << '\t' << real(vfseries[ifreq]) << std::endl;
   }
   
 }
