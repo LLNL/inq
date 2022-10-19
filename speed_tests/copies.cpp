8d0383c616718d7cd4abfff0867d42ba377fda99
Alfredo Correa <correaa@llnl.gov>
2022-10-19 11:47:10 -0700

    fix nonsense i variable

--- speed_tests/copies.cpp
+++ speed_tests/copies.cpp
@@ -39,10 +39,8 @@ TEST_CASE("speed_test::copy", "[speed_test::copy]") {
 			long nn = 8000;
 
 			// WARMUP POOL MEMORY, IF ANY
-			std::vector<math::array<complex, 2>> warmup(10, math::array<complex, 2>({nn, nn}, static_cast<double>(i)));
-			for(int i = 0; i != 10; ++i) {
-				CHECK(warmup[5][nn-1][nn-1] == complex{static_cast<double>(i)});
-			}
+			std::vector<math::array<complex, 2>> warmup(10, math::array<complex, 2>({nn, nn}, complex{1.0, 2.0}));
+			CHECK( warmup[5][nn-1][nn-1] == complex{1.0, 2.0} );
 
 			math::array<complex, 2> src({nn, nn});
 			math::array<complex, 2> dest({nn, nn});
