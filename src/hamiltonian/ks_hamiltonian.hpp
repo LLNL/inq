41d872c7cd17bbc0ba3f4822b21b98aca121d7e0
Xavier Andrade <xavier@tddft.org>
2022-12-05 14:29:18 -0800

    Remove the projectors list as a member of hamiltonian.

--- src/hamiltonian/ks_hamiltonian.hpp
+++ src/hamiltonian/ks_hamiltonian.hpp
@@ -51,8 +51,9 @@ namespace hamiltonian {
 		void update_projectors(const basis_type & basis, const ions::unit_cell & cell, const atomic_potential & pot, const ions::geometry & geo){
 			
 			CALI_CXX_MARK_FUNCTION;
+
+			std::list<projector> projectors;
 			
-			projectors_.clear();
 			projectors_fourier_map_.clear();			
 			
 			for(int iatom = 0; iatom < geo.num_atoms(); iatom++){
@@ -61,12 +62,12 @@ namespace hamiltonian {
 																												projector_fourier(basis, cell, pot.pseudo_for_element(geo.atoms()[iatom])));
 					insert.first->second.add_coord(basis.cell().metric().to_contravariant(geo.coordinates()[iatom]));
 				} else {
-					projectors_.emplace_back(basis, cell, pot.pseudo_for_element(geo.atoms()[iatom]), geo.coordinates()[iatom], iatom);
-					if(projectors_.back().empty()) projectors_.pop_back(); 
+					projectors.emplace_back(basis, cell, pot.pseudo_for_element(geo.atoms()[iatom]), geo.coordinates()[iatom], iatom);
+					if(projectors.back().empty()) projectors.pop_back(); 
 				}
 			}
 
-			projectors_all_ = projector_all(projectors_);
+			projectors_all_ = projector_all(projectors);
 			
 		}
 		
@@ -188,7 +189,6 @@ namespace hamiltonian {
 		
   private:
 
-		std::list<projector> projectors_;
 		projector_all projectors_all_;		
 		bool non_local_in_fourier_;
 		std::unordered_map<std::string, projector_fourier> projectors_fourier_map_;
