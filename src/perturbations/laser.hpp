520c9fb0a29e9aee95ee8360afd982e0b6487269
Xavier Andrade <xavier@tddft.org>
2023-01-11 08:21:43 -0800

    Fix indentation.

--- src/perturbations/laser.hpp
+++ src/perturbations/laser.hpp
@@ -32,50 +32,50 @@ namespace perturbations {
 
 class laser {
 
-    math::vector3<double, math::cartesian> polarization_;
-    double frequency_;
-
+	math::vector3<double, math::cartesian> polarization_;
+	double frequency_;
+	
 public:
-    laser(math::vector3<double, math::cartesian> polarization, quantity<magnitude::energy> frequency,gauge arg_gauge = gauge::length):
-        polarization_(polarization),
-        frequency_(frequency.in_atomic_units()),
-        gauge_(arg_gauge) {
-            assert(gauge_ != gauge::mixed);
-    }
-
-    template <typename DummyType>
-    void zero_step(DummyType &) const {
-    }
-
-    auto has_uniform_electric_field() const {
-        return gauge_ == gauge::length;
-    }
-
-    auto uniform_electric_field(double time) const {
-        return polarization_*sin(time*frequency_);
-    }
-
-    auto has_uniform_vector_potential() const {
-        return gauge_ == gauge::velocity;
-    }
-
-    auto uniform_vector_potential(double time) const {
-        //E=-1/c*dA/dt
-        return polarization_/frequency_*(cos(time*frequency_) - 1.0);
-    }
-
-    template <typename OutputStream>
-    void print_info(OutputStream & out) {
-        auto freq_ev = frequency_*27.211383;
-
-        out << "Frequency :    " << frequency_ << " Ha" << std::endl;
-        out << "               " << freq_ev << " eV" << std::endl;
-        out << "               " << freq_ev*241.7991 << " THz" << std::endl;
-        out << "               " << 1239.84193/freq_ev << " nm" << std::endl;
-
-    }
+	laser(math::vector3<double, math::cartesian> polarization, quantity<magnitude::energy> frequency,gauge arg_gauge = gauge::length):
+		polarization_(polarization),
+		frequency_(frequency.in_atomic_units()),
+		gauge_(arg_gauge) {
+		assert(gauge_ != gauge::mixed);
+	}
+	
+	template <typename DummyType>
+	void zero_step(DummyType &) const {
+	}
+	
+	auto has_uniform_electric_field() const {
+		return gauge_ == gauge::length;
+	}
+	
+	auto uniform_electric_field(double time) const {
+		return polarization_*sin(time*frequency_);
+	}
+	
+	auto has_uniform_vector_potential() const {
+		return gauge_ == gauge::velocity;
+	}
+	
+	auto uniform_vector_potential(double time) const {
+		//E=-1/c*dA/dt
+		return polarization_/frequency_*(cos(time*frequency_) - 1.0);
+	}
+	
+	template <typename OutputStream>
+	void print_info(OutputStream & out) {
+		auto freq_ev = frequency_*27.211383;
+		
+		out << "Frequency :    " << frequency_ << " Ha" << std::endl;
+		out << "               " << freq_ev << " eV" << std::endl;
+		out << "               " << freq_ev*241.7991 << " THz" << std::endl;
+		out << "               " << 1239.84193/freq_ev << " nm" << std::endl;
+		
+	}
 private:
-    gauge gauge_;
+	gauge gauge_;
 
 };
 
@@ -100,24 +100,26 @@ using math::vector3;
 
 TEST_CASE("perturbations::laser", "[perturbations::laser]") {
 
-    perturbations::laser las({1.0, 0.0, 0.0}, 1.0_eV);
+	perturbations::laser las({1.0, 0.0, 0.0}, 1.0_eV);
 
-    las.print_info(std::cout);
+	las.print_info(std::cout);
+	
+	CHECK(las.has_uniform_electric_field());
 
-    CHECK(las.has_uniform_electric_field());
-    SECTION("velocity gauge"){
-        perturbations::laser vector_potential({0.1, 0.0, 0.0}, 1.0_eV,perturbations::gauge::velocity);
+	SECTION("velocity gauge"){
+		perturbations::laser vector_potential({0.1, 0.0, 0.0}, 1.0_eV,perturbations::gauge::velocity);
 		CHECK(vector_potential.has_uniform_vector_potential());
 		CHECK(not vector_potential.has_uniform_electric_field());
 		CHECK(vector_potential.uniform_vector_potential(0.0)[0] == 0.0);
 		CHECK(vector_potential.uniform_vector_potential(0.0)[2] == 0.0);
-    }
-    SECTION("length gauge"){
-        perturbations::laser E_field({1.0, 0.0, 0.0}, 1.0_eV);
-        CHECK(E_field.has_uniform_electric_field());
+	}
+
+	SECTION("length gauge"){
+		perturbations::laser E_field({1.0, 0.0, 0.0}, 1.0_eV);
+		CHECK(E_field.has_uniform_electric_field());
 		CHECK(not E_field.has_uniform_vector_potential());
-        CHECK(E_field.uniform_electric_field(0.0)[0] == 0.0);
-    }
+		CHECK(E_field.uniform_electric_field(0.0)[0] == 0.0);
+	}
 }
 
 #endif
