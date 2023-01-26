d77ebe2ab6d37234089fd997920e1b91a85e3aee
Xavier Andrade <xavier@tddft.org>
2023-01-26 13:51:14 -0800

    Add a class to return zero for different types.

---
+++ src/math/zero.hpp
@@ -0,0 +1,66 @@
+/* -*- indent-tabs-mode: t -*- */
+
+#ifndef INQ__MATH__ZERO
+#define INQ__MATH__ZERO
+
+/*
+ Copyright (C) 2019 Xavier Andrade
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
+  
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+  
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <cassert>
+#include <array>
+#include <cmath>
+
+#include <tinyformat/tinyformat.h>
+
+namespace inq {
+
+template <typename Type>
+auto zero(){
+  return Type{0.0};
+}
+
+template <>
+auto zero<math::vector3<double>>(){
+  return math::vector3<double>{0.0, 0.0, 0.0};
+}
+
+template <>
+auto zero<math::vector3<double, math::covariant>>(){
+  return math::vector3<double, math::covariant>{0.0, 0.0, 0.0};
+}
+
+template <>
+auto zero<math::vector3<double, math::contravariant>>(){
+  return math::vector3<double, math::contravariant>{0.0, 0.0, 0.0};
+}
+
+}
+
+#ifdef INQ_MATH_ZERO_UNIT_TEST
+#undef INQ_MATH_ZERO_UNIT_TEST
+
+#include <catch2/catch_all.hpp>
+
+TEST_CASE("class math::match", "[math::match]") {
+  
+}
+
+#endif
+
+    
+#endif
