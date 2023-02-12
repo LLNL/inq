814bcfa6e4d12c5ae68ab97ff6549f7a7b3966f6
Xavier Andrade <xavier@tddft.org>
2023-02-12 12:44:31 -0800

    Use all orbitals for the exchange and store them in a field_set.

--- src/basis/field_set.hpp
+++ src/basis/field_set.hpp
@@ -53,10 +53,10 @@ public:
 	typedef math::array<ElementType, 2> internal_array_type;
 	typedef ElementType element_type;
 
-	field_set(const basis_type & basis, PartitionType && part, parallel::cartesian_communicator<2> comm)
+	field_set(const basis_type & basis, PartitionType part, parallel::cartesian_communicator<2> comm)
 		:full_comm_(std::move(comm)),
 		 set_comm_(basis::set_subcomm(full_comm_)),
-		 set_part_(part),
+		 set_part_(std::move(part)),
 		 matrix_({basis.part().local_size(), set_part_.local_size()}),
 		 num_vectors_(set_part_.size()),
 		 basis_(basis)
