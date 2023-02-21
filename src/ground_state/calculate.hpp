d812cace88fa97dcff37b53970ba94c134793207
Xavier Andrade <xavier@tddft.org>
2023-02-21 01:56:30 -0800

    Add a copyright notice to ground_state::calculate.

--- src/ground_state/calculate.hpp
+++ src/ground_state/calculate.hpp
@@ -1,9 +1,25 @@
 /* -*- indent-tabs-mode: t -*- */
 
-//  Copyright (C) 2019-2023 Xavier Andrade, Alfredo A. Correa
-
 #ifndef INQ__GROUND_STATE__CALCULATE
 #define INQ__GROUND_STATE__CALCULATE
+
+/*
+ Copyright (C) 2019-2023 Xavier Andrade, Alfredo A. Correa
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
+  
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+  
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 #include <cfloat>
 
