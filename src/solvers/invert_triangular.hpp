1c9f7d9edb850caeb784ed8413518c53f9148a9f
Xavier Andrade <xavier@tddft.org>
2023-02-23 15:07:10 -0800

    Working version of invert_triangular.

--- src/solvers/invert_triangular.hpp
+++ src/solvers/invert_triangular.hpp
@@ -31,6 +31,8 @@
 #include <math/subspace_matrix.hpp>
 #include <utils/raw_pointer_cast.hpp>
 
+#include <cusolverDn.h>
+
 #define dtrtri FC_GLOBAL(dtrtri, DTRTRI) 
 extern "C" void  dtrtri(const char * uplo, const char * diag, const int * n, double * a, const int * lda, int * info);
 
@@ -44,15 +46,59 @@ template <typename Type>
 void invert_triangular(math::subspace_matrix<Type> & matrix){
   CALI_CXX_MARK_SCOPE("invert_triangular_double");
 
+	static_assert(std::is_same_v<Type, double> or std::is_same_v<Type, complex>, "invert_triangular is only implemented for double and complex");
+	
 	int nn = std::get<0>(sizes(matrix.array()));
+
+	auto matrix_data = raw_pointer_cast(matrix.array().data_elements());
 	int info;
+	
+#ifndef ENABLE_CUDA	
 
 	if constexpr (std::is_same_v<Type, double>){
-		dtrtri("U", "N", &nn, raw_pointer_cast(matrix.array().data_elements()), &nn, &info);
+		dtrtri("U", "N", &nn, matrix_data, &nn, &info);
 	} else {
-		static_assert(std::is_same_v<Type, complex>, "invert_triangular is only implemented for double and complex");
-		ztrtri("U", "N", &nn, raw_pointer_cast(matrix.array().data_elements()), &nn, &info);
+		ztrtri("U", "N", &nn, matrix_data, &nn, &info);
 	}
+
+#else
+
+	cudaDataType datatype = CUDA_C_64F;
+	if constexpr (std::is_same_v<Type, double>){
+		datatype = CUDA_R_64F;
+	}
+	
+	std::cout << CUSOLVER_STATUS_SUCCESS << '\t' << CUSOLVER_STATUS_NOT_INITIALIZED << '\t' << CUSOLVER_STATUS_NOT_SUPPORTED << '\t' << CUSOLVER_STATUS_INVALID_VALUE << '\t' << CUSOLVER_STATUS_INTERNAL_ERROR << std::endl;
+	
+	cusolverDnHandle_t cusolver_handle;
+	
+	[[maybe_unused]] auto cusolver_status = cusolverDnCreate(&cusolver_handle);
+	assert(CUSOLVER_STATUS_SUCCESS == cusolver_status);
+	
+	size_t worksize_host, worksize_device;
+
+	cusolver_status = cusolverDnXtrtri_bufferSize(cusolver_handle, CUBLAS_FILL_MODE_UPPER, CUBLAS_DIAG_NON_UNIT, nn, datatype, matrix_data, nn, &worksize_device, &worksize_host);
+	std::cout << cusolver_status << std::endl;
+	assert(CUSOLVER_STATUS_SUCCESS == cusolver_status);
+
+	std::cout << worksize_device << '\t' << worksize_host << std::endl;
+
+	void * work_device;
+	cudaMalloc(&work_device, worksize_device);
+	//math::array<char, 1> work_device(worksize_device);
+	//	assert(work_device.num_elements() == worksize_device);
+
+	auto work_host = malloc(worksize_host);
+	assert(work_host != NULL);
+	
+	cusolver_status = cusolverDnXtrtri(cusolver_handle, CUBLAS_FILL_MODE_UPPER, CUBLAS_DIAG_NON_UNIT, nn, datatype, matrix_data, nn, work_device, worksize_device, work_host, worksize_host, &info);
+	std::cout << cusolver_status << '\t' << info << std::endl;	
+	assert(CUSOLVER_STATUS_SUCCESS == cusolver_status);
+
+	free(work_host);
+	cusolverDnDestroy(cusolver_handle);
+	
+#endif
 	
 	gpu::run(nn, nn, [mat = begin(matrix.array())] GPU_LAMBDA (auto ii, auto jj){
 		if(ii < jj) mat[ii][jj] = 0.0;
@@ -80,7 +126,7 @@ TEST_CASE(INQ_TEST_FILE, INQ_TEST_TAG) {
 
   auto comm = boost::mpi3::environment::get_world_instance();
 	inq::parallel::cartesian_communicator<2> cart_comm(comm, {});
-  
+
 	SECTION("Real 2x2"){
 	
 		using namespace inq;
@@ -118,6 +164,45 @@ TEST_CASE(INQ_TEST_FILE, INQ_TEST_TAG) {
     CHECK(matrix.array()[1][0] == 0.125);
     CHECK(matrix.array()[1][1] == 0.5);
   }
-  
+	
+  SECTION("Complex 3x3"){
+	
+		using namespace inq;
+		using namespace Catch::literals;
+
+    math::subspace_matrix<complex> matrix(cart_comm, 3);
+		
+		matrix.array()[0][0] = 4.0;
+		matrix.array()[0][1] = 0.0;
+		matrix.array()[0][2] = 0.0;
+		matrix.array()[1][0] = 0.0;
+		matrix.array()[1][1] = 10.0;
+		matrix.array()[1][2] = 0.0;
+		matrix.array()[2][0] = 0.0;
+		matrix.array()[2][1] = 0.0;
+		matrix.array()[2][2] = 3.0;		
+
+		solvers::invert_triangular(matrix);
+
+  }
+	
+	SECTION("Complex NxN"){
+	
+		using namespace inq;
+		using namespace Catch::literals;
+
+		auto nn = 100;
+
+    math::subspace_matrix<complex> matrix(cart_comm, nn);
+		
+		for(int ii = 0; ii < nn; ii++){
+			for(int jj = 0; jj < nn; jj++){
+				matrix.array()[ii][jj] = (ii == jj) ? 1.0 : 0.0;
+			}
+		}
+		
+		solvers::invert_triangular(matrix);
+		
+  }
 }
 #endif
