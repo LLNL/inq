d73d98c553a78af4159e5db2d28818e7ed8f57fb
Xavier Andrade <xavier@tddft.org>
2023-04-12 23:57:09 -0700

    Converted many headers to MPL.

--- src/ground_state/initial_guess.hpp
+++ src/ground_state/initial_guess.hpp
@@ -3,6 +3,12 @@
 #ifndef INQ__GROUND_STATE__INITIAL_GUESS
 #define INQ__GROUND_STATE__INITIAL_GUESS
 
+// Copyright (C) 2019-2023 Lawrence Livermore National Security, LLC., Xavier Andrade, Alfredo A. Correa
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 #include <cfloat>
 
 #include <systems/ions.hpp>
--- src/ground_state/result.hpp
+++ src/ground_state/result.hpp
@@ -3,6 +3,12 @@
 #ifndef INQ__GROUND_STATE__RESULT
 #define INQ__GROUND_STATE__RESULT
 
+// Copyright (C) 2019-2023 Lawrence Livermore National Security, LLC., Xavier Andrade, Alfredo A. Correa
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 #include <hamiltonian/energy.hpp>
 #include <math/array.hpp>
 #include <math/vector3.hpp>
