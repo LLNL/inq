83f833764527de48daa8f07163637228f91bc14e
Xavier Andrade <xavier@tddft.org>
2023-05-11 13:22:02 -0700

    Added a cholesky operation to diagonalization. It calls the serial version for now.

---
+++ src/matrix/cholesky.hpp
@@ -0,0 +1,80 @@
+/* -*- indent-tabs-mode: t -*- */
+
+#ifndef INQ__MATRIX__CHOLESKY
+#define INQ__MATRIX__CHOLESKY
+
+// Copyright (C) 2019-2023 Lawrence Livermore National Security, LLC., Xavier Andrade, Alfredo A. Correa
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include <inq_config.h>
+#include <matrix/gather_scatter.hpp>
+#include <solvers/cholesky.hpp>
+
+namespace inq {
+namespace matrix {
+
+template <typename DistributedMatrix>
+void cholesky(DistributedMatrix & matrix) {
+
+  assert(matrix.sizex() == matrix.sizey());
+  
+  auto full_matrix = matrix::gather(matrix);
+  if(matrix.comm().root()) solvers::cholesky(full_matrix);
+  matrix = matrix::scatter(matrix.comm(), full_matrix);
+
+}
+
+}
+}
+#endif
+
+///////////////////////////////////////////////////////////////////
+
+#ifdef INQ_MATRIX_CHOLESKY_UNIT_TEST
+#undef INQ_MATRIX_CHOLESKY_UNIT_TEST
+
+#include <gpu/array.hpp>
+#include <matrix/distributed.hpp>
+
+#include <catch2/catch_all.hpp>
+
+TEST_CASE(INQ_TEST_FILE, INQ_TEST_TAG) {
+
+  using namespace inq;
+  using namespace Catch::literals;
+  
+	parallel::communicator comm{boost::mpi3::environment::get_world_instance()};
+	parallel::cartesian_communicator<2> cart_comm(comm, {});
+
+
+  SECTION("Complex 2x2"){
+    
+		using namespace inq;
+    using namespace Catch::literals;
+		
+		gpu::array<complex, 2> array({2, 2});
+		
+		array[0][0] = 6432.12;
+		array[0][1] = 4502.48;
+		array[1][0] = 4502.48;
+		array[1][1] = 3151.74;
+
+		matrix::distributed matrix = matrix::scatter(cart_comm, array);
+		
+		matrix::cholesky(matrix);
+
+    array = matrix::gather(matrix);
+
+    if(cart_comm.root()){
+      CHECK(real(array[0][0]) == 80.2005_a);
+      CHECK(real(array[0][1]) == 0.0_a);
+      CHECK(real(array[1][0]) == 56.1402992511_a);
+      CHECK(real(array[1][1]) == 0.0824620974_a);    
+		}
+  }
+
+}
+#endif
