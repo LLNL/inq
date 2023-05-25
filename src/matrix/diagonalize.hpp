bbe70a82440fdfe2034eaeaff7bbd5b7f4ce3ac5
Xavier Andrade <xavier@tddft.org>
2023-05-25 01:32:50 -0700

    Removed operations::diagonalize.

--- src/matrix/diagonalize.hpp
+++ src/matrix/diagonalize.hpp
@@ -10,11 +10,158 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 #include <inq_config.h>
+#include <math/complex.hpp>
 #include <matrix/gather_scatter.hpp>
-#include <operations/diagonalize.hpp>
+
+#include <inq_config.h>
+
+#ifdef ENABLE_CUDA
+#include <cusolverDn.h>
+#endif
+
+#include "FC.h"
+
+#include <utils/profiling.hpp>
+
+#define dsyev FC_GLOBAL(dsyev, DZYEV)
+extern "C" void dsyev(const char * jobz, const char * uplo, const int & n, double * a, const int & lda, double * w, double * work, const int & lwork, int & info);
+
+#define zheev FC_GLOBAL(zheev, ZHEEV) 
+extern "C" void zheev(const char * jobz, const char * uplo, const int & n, inq::complex * a, const int & lda, double * w, inq::complex * work, const int & lwork, double * rwork, int & info);
 
 namespace inq {
 namespace matrix {
+
+template<class Alloc>
+auto diagonalize_raw(gpu::array<double, 2, Alloc>& matrix){
+
+	CALI_CXX_MARK_FUNCTION;
+
+	// the matrix must be square
+	assert(std::get<0>(sizes(matrix)) == std::get<1>(sizes(matrix)));
+
+	int nn = std::get<0>(sizes(matrix));
+    
+	gpu::array<double, 1> eigenvalues(nn);
+
+#ifdef ENABLE_CUDA
+	{
+		cusolverDnHandle_t cusolver_handle;
+			
+		[[maybe_unused]] auto cusolver_status = cusolverDnCreate(&cusolver_handle);
+		assert(CUSOLVER_STATUS_SUCCESS == cusolver_status);
+			
+		//query the work size
+		int lwork;
+		cusolver_status = cusolverDnDsyevd_bufferSize(cusolver_handle, CUSOLVER_EIG_MODE_VECTOR, CUBLAS_FILL_MODE_UPPER, nn,
+																									raw_pointer_cast(matrix.data_elements()), nn, raw_pointer_cast(eigenvalues.data_elements()), &lwork);
+		assert(cusolver_status == CUSOLVER_STATUS_SUCCESS);
+		assert(lwork >= 0);
+
+		//allocate the work array
+		double * work;
+		[[maybe_unused]] auto cuda_status = cudaMalloc((void**)&work, sizeof(double)*lwork);
+		assert(cudaSuccess == cuda_status);
+
+		//finally, diagonalize
+		int * devInfo;
+		cuda_status = cudaMallocManaged((void**)&devInfo, sizeof(int));
+		assert(cudaSuccess == cuda_status);
+			
+		cusolver_status = cusolverDnDsyevd(cusolver_handle, CUSOLVER_EIG_MODE_VECTOR, CUBLAS_FILL_MODE_UPPER, nn,
+																			 raw_pointer_cast(matrix.data_elements()), nn, raw_pointer_cast(eigenvalues.data_elements()), work, lwork, devInfo);
+
+		assert(cusolver_status == CUSOLVER_STATUS_SUCCESS);
+		cudaDeviceSynchronize();
+		assert(*devInfo == 0);
+
+		cudaFree(work);
+		cudaFree(devInfo);
+		cusolverDnDestroy(cusolver_handle);
+	}
+#else
+	double lwork_query;
+
+	int info;
+	dsyev("V", "U", nn, matrix.data_elements(), nn, eigenvalues.data_elements(), &lwork_query, -1, info);
+
+	int lwork = int(lwork_query);
+	auto work = (double *) malloc(lwork*sizeof(complex));
+    
+	dsyev("V", "U", nn, matrix.data_elements(), nn, eigenvalues.data_elements(), work, lwork, info);
+	assert(info == 0);
+		
+	free(work);
+#endif
+
+	return eigenvalues;
+}
+
+template<class Alloc>
+auto diagonalize_raw(gpu::array<complex, 2, Alloc>& matrix){
+
+	CALI_CXX_MARK_FUNCTION;
+	
+	// the matrix must be square
+	assert(std::get<0>(sizes(matrix)) == std::get<1>(sizes(matrix)));
+
+	int nn = std::get<0>(sizes(matrix));
+    
+	gpu::array<double, 1> eigenvalues(nn);
+
+#ifdef ENABLE_CUDA
+	{
+		cusolverDnHandle_t cusolver_handle;
+			
+		[[maybe_unused]] auto cusolver_status = cusolverDnCreate(&cusolver_handle);
+		assert(CUSOLVER_STATUS_SUCCESS == cusolver_status);
+			
+		//query the work size
+		int lwork;
+		cusolver_status = cusolverDnZheevd_bufferSize(cusolver_handle, CUSOLVER_EIG_MODE_VECTOR, CUBLAS_FILL_MODE_UPPER, nn,
+																									(cuDoubleComplex const *) raw_pointer_cast(matrix.data_elements()), nn, raw_pointer_cast(eigenvalues.data_elements()), &lwork);
+		assert(cusolver_status == CUSOLVER_STATUS_SUCCESS);
+		assert(lwork >= 0);
+
+		//allocate the work array
+		cuDoubleComplex * work;
+		[[maybe_unused]] auto cuda_status = cudaMalloc((void**)&work, sizeof(cuDoubleComplex)*lwork);
+		assert(cudaSuccess == cuda_status);
+
+		//finally, diagonalize
+		int * devInfo;
+		cuda_status = cudaMallocManaged((void**)&devInfo, sizeof(int));
+		assert(cudaSuccess == cuda_status);
+			
+		cusolver_status = cusolverDnZheevd(cusolver_handle, CUSOLVER_EIG_MODE_VECTOR, CUBLAS_FILL_MODE_UPPER, nn,
+																			 (cuDoubleComplex *) raw_pointer_cast(matrix.data_elements()), nn, raw_pointer_cast(eigenvalues.data_elements()), work, lwork, devInfo);
+		assert(cusolver_status == CUSOLVER_STATUS_SUCCESS);
+		cudaDeviceSynchronize();
+		assert(*devInfo == 0);
+			
+		cudaFree(devInfo);
+		cusolverDnDestroy(cusolver_handle);
+	}
+#else
+	complex lwork_query;
+
+	auto rwork = (double *) malloc(std::max(1, 3*nn - 2)*sizeof(double));
+    
+	int info;
+	zheev("V", "U", nn, matrix.data_elements(), nn, eigenvalues.data_elements(), &lwork_query, -1, rwork, info);
+
+
+	int lwork = int(real(lwork_query));
+	auto work = (complex *) malloc(lwork*sizeof(complex));
+    
+	zheev("V", "U", nn, matrix.data_elements(), nn, eigenvalues.data_elements(), work, lwork, rwork, info);
+    
+	free(rwork);
+	free(work);
+#endif
+		
+	return eigenvalues;
+}
 
 template <typename DistributedMatrix>
 auto diagonalize(DistributedMatrix & matrix) {
@@ -26,7 +173,7 @@ auto diagonalize(DistributedMatrix & matrix) {
   auto full_matrix = matrix::gather(matrix, /* root = */ 0);
   
   if(matrix.comm().root()) {
-    eigenvalues = operations::diagonalize_raw(full_matrix);
+    eigenvalues = diagonalize_raw(full_matrix);
   } else {
     eigenvalues = gpu::array<double, 1>(matrix.sizex());
   }
