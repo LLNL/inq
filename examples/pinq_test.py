01d9628840f64e56bc962b25972fc7ce4b80b49e
Xavier Andrade <xavier@tddft.org>
2023-09-11 15:28:20 -0700

    Add a python script to test.

---
+++ examples/pinq_test.py
@@ -0,0 +1,10 @@
+from ase import Atoms
+from ase.optimize import BFGS
+from ase.calculators.nwchem import NWChem
+from ase.io import write
+
+h2 = Atoms('H2', positions=[[0, 0, 0], [0.0, 0, 0.7]])
+
+import pinq
+
+pinq.run(h2)
