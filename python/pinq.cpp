875b2d4e57a71daf8ff34364aa4844a0bb87d45b
Xavier Andrade <xavier@tddft.org>
2023-09-11 15:20:06 -0700

    Receive an ASE Atoms object.

--- python/pinq.cpp
+++ python/pinq.cpp
@@ -9,32 +9,38 @@
 #include <inq/inq.hpp>
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/numpy.h>
 
 namespace py = pybind11;
+using namespace pybind11::literals;
 
-void run(){
+void run(py::object atoms){
 
 	using namespace inq;
 	using namespace inq::magnitude;
-	
+
 	input::environment env{};
 	
 	utils::match energy_match(6.0e-6);
 
+	auto atomic_numbers = atoms.attr("get_atomic_numbers")().cast<py::array_t<int>>();
+	auto num = static_cast<int *>(atomic_numbers.request().ptr);
+	auto positions = atoms.attr("get_positions")().cast<py::array_t<double>>();
+	auto pos = static_cast<double *>(positions.request().ptr);
+
 	systems::ions ions(systems::cell::orthorhombic(10.0_b, 10.0_b, 12.0_b));
 
-	auto distance = 2.2_bohr; //a bit larger than experiment to check the force
+	for(int ii = 0; ii < atomic_numbers.size(); ii++){
+		ions.insert(num[ii], 1.0_A*vector3{pos[3*ii + 0], pos[3*ii + 1], pos[3*ii + 2]});
+	}
 	
-	ions.insert("N", {0.0_b, 0.0_b, -0.5*distance});
-	ions.insert("N", {0.0_b, 0.0_b,  0.5*distance});
-
 	systems::electrons electrons(env.par(), ions, options::electrons{}.cutoff(40.0_Ha));
 	ground_state::initial_guess(ions, electrons);
 	
 	auto result = ground_state::calculate(ions, electrons, options::theory{}.lda(), options::ground_state{}.energy_tolerance(1e-9_Ha).calculate_forces());
 
 	std::cout << "Energy = " << result.energy.total() << std::endl;
-	
 }
 
 PYBIND11_MODULE(pinq, module) {
