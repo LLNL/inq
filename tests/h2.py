def41aff5cf424e55539263c6af7005b998579ff
Xavier Andrade <xavier@tddft.org>
2023-09-12 07:28:23 -0700

    Add a python test for h2.

---
+++ tests/h2.py
@@ -0,0 +1,12 @@
+from ase import Atoms
+from ase.optimize import BFGS
+from ase.calculators.nwchem import NWChem
+from ase.io import write
+import pinq
+
+h2 = Atoms('H2', positions=[[0, 0, 0], [0.0, 0, 0.7]], cell = [3.0, 3.0, 4.0])
+energy = pinq.run(h2)
+
+print("Energy = ", energy);
+
+assert abs(energy - -1.2236909610508657) < 3.0e-5
