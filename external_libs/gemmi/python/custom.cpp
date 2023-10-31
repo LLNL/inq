e554222234ee25de60f1dffb1cf6b9ba99bde6e4
Alfredo Correa <alfredo.correa+work@gmail.com>
2023-10-31 15:03:23 -0700

    remove gemmi

--- external_libs/gemmi/python/custom.cpp
+++
@@ -1,9 +0,0 @@
-// An empty file that makes easier to add custom functions.
-//
-
-#include "common.h"  // it includes <pybind11/pybind11.h>
-namespace py = pybind11;
-
-void add_custom(py::module& m) {
-  (void) m;
-}
