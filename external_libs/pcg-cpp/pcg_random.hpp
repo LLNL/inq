91b40a6b281a88617cabe067de0d435711f37a07
Xavier Andrade <xavier@tddft.org>
2023-12-19 23:26:12 -0800

    Use GPU_FUNCTION in PCG

--- external_libs/pcg-cpp/pcg_random.hpp
+++ external_libs/pcg-cpp/pcg_random.hpp
@@ -113,6 +113,8 @@
 
 #include "pcg_extras.hpp"
 
+#include <gpu/run.hpp>
+
 namespace pcg_detail {
 
 using namespace pcg_extras;
@@ -411,25 +413,19 @@ public:
 
 protected:
 
-#ifdef __CUDACC__
-    __host__ __device__
-#endif
+GPU_FUNCTION
     itype bump(itype state)
     {
         return state * multiplier() + increment();
     }
 
-#ifdef __CUDACC__
-    __host__ __device__
-#endif
+GPU_FUNCTION
     itype base_generate()
     {
         return state_ = bump(state_);
     }
 
-#ifdef __CUDACC__
-    __host__ __device__
-#endif
+GPU_FUNCTION
     itype base_generate0()
     {
         itype old_state = state_;
@@ -438,9 +434,7 @@ protected:
     }
 
 public:
-#ifdef __CUDACC__
-    __host__ __device__
-#endif
+GPU_FUNCTION
     result_type operator()()
     {
         if (output_previous)
@@ -449,18 +443,14 @@ public:
             return this->output(base_generate());
     }
 
-#ifdef __CUDACC__ 
-    __host__  __device__
-#endif
+GPU_FUNCTION
     result_type operator()(result_type upper_bound)
     {
         return bounded_rand(*this, upper_bound);
     }
 
 protected:
-#ifdef __CUDACC__
-    __host__ __device__
-#endif
+GPU_FUNCTION
     static itype advance(itype state, itype delta,
                          itype cur_mult, itype cur_plus);
 
@@ -473,9 +463,7 @@ protected:
     }
 
 public:
-#ifdef __CUDACC__
-    __host__ __device__
-#endif
+GPU_FUNCTION
     void advance(itype delta)
     {
         state_ = advance(state_, delta, this->multiplier(), this->increment());
@@ -486,9 +474,7 @@ public:
         advance(-delta);
     }
 
-#ifdef __CUDACC__
-    __host__ __device__
-#endif
+GPU_FUNCTION
     void discard(itype delta)
     {
         advance(delta);
@@ -506,9 +492,7 @@ public:
         }
     }
 
-#ifdef __CUDACC__
-    __host__ __device__
-#endif
+GPU_FUNCTION
     engine(itype state = itype(0xcafef00dd15ea5e5ULL))
         : state_(this->is_mcg ? state|state_type(3U)
                               : bump(state + this->increment()))
@@ -519,9 +503,7 @@ public:
     // This function may or may not exist.  It thus has to be a template
     // to use SFINAE; users don't have to worry about its template-ness.
     template <typename sm = stream_mixin>
-#ifdef __CUDACC__
-    __host__ __device__
-#endif
+GPU_FUNCTION
     engine(itype state, typename sm::stream_state stream_seed)
         : stream_mixin(stream_seed),
           state_(this->is_mcg ? state|state_type(3U)
@@ -531,9 +513,7 @@ public:
     }
 
     template<typename SeedSeq>
-#ifdef __CUDACC__
-    __host__ __device__
-#endif
+GPU_FUNCTION
     engine(SeedSeq&& seedSeq, typename std::enable_if<
                   !stream_mixin::can_specify_stream
                && !std::is_convertible<SeedSeq, itype>::value
@@ -545,9 +525,7 @@ public:
     }
 
     template<typename SeedSeq>
-#ifdef __CUDACC__
-    __host__ __device__
-#endif    
+GPU_FUNCTION    
     engine(SeedSeq&& seedSeq, typename std::enable_if<
                    stream_mixin::can_specify_stream
                && !std::is_convertible<SeedSeq, itype>::value
@@ -676,9 +654,7 @@ operator>>(std::basic_istream<CharT,Traits>& in,
 template <typename xtype, typename itype,
           typename output_mixin, bool output_previous,
           typename stream_mixin, typename multiplier_mixin>
-#ifdef __CUDACC__
-__host__ __device__
-#endif
+GPU_FUNCTION
 itype engine<xtype,itype,output_mixin,output_previous,stream_mixin,
              multiplier_mixin>::advance(
     itype state, itype delta, itype cur_mult, itype cur_plus)
@@ -849,9 +825,7 @@ using mcg_base = engine<xtype, itype,
 
 template <typename xtype, typename itype>
 struct xsh_rs_mixin {
-#ifdef __CUDACC__
-    __host__ __device__
-#endif
+GPU_FUNCTION
     static xtype output(itype internal)
     {
         constexpr bitcount_t bits        = bitcount_t(sizeof(itype) * 8);
@@ -885,9 +859,7 @@ struct xsh_rs_mixin {
 
 template <typename xtype, typename itype>
 struct xsh_rr_mixin {
-#ifdef __CUDACC__
-    __host__ __device__
-#endif
+GPU_FUNCTION
     static xtype output(itype internal)
     {
         constexpr bitcount_t bits        = bitcount_t(sizeof(itype) * 8);
@@ -923,9 +895,7 @@ struct xsh_rr_mixin {
 
 template <typename xtype, typename itype>
 struct rxs_mixin {
-#ifdef __CUDACC__
-__host__ __device__
-#endif  
+GPU_FUNCTION
 static xtype output_rxs(itype internal)
     {
         constexpr bitcount_t bits        = bitcount_t(sizeof(itype) * 8);
@@ -992,9 +962,7 @@ PCG_DEFINE_CONSTANT(pcg128_t, mcg, unmultiplier,
 
 template <typename xtype, typename itype>
 struct rxs_m_xs_mixin {
-#ifdef __CUDACC__
-    __host__ __device__
-#endif
+GPU_FUNCTION
     static xtype output(itype internal)
     {
         constexpr bitcount_t xtypebits = bitcount_t(sizeof(xtype) * 8);
@@ -1043,9 +1011,7 @@ struct rxs_m_xs_mixin {
 
 template <typename xtype, typename itype>
 struct rxs_m_mixin {
-#ifdef __CUDACC__
-    __host__ __device__
-#endif  
+GPU_FUNCTION  
     static xtype output(itype internal)
     {
         constexpr bitcount_t xtypebits = bitcount_t(sizeof(xtype) * 8);
@@ -1084,9 +1050,7 @@ struct rxs_m_mixin {
 
 template <typename xtype, typename itype>
 struct dxsm_mixin {
-#ifdef __CUDACC__
-    __host__ __device__
-#endif
+GPU_FUNCTION
     inline xtype output(itype internal)
     {
         constexpr bitcount_t xtypebits = bitcount_t(sizeof(xtype) * 8);
@@ -1115,9 +1079,7 @@ struct dxsm_mixin {
 
 template <typename xtype, typename itype>
 struct xsl_rr_mixin {
-#ifdef __CUDACC__
-    __host__ __device__
-#endif
+GPU_FUNCTION
     static xtype output(itype internal)
     {
         constexpr bitcount_t xtypebits = bitcount_t(sizeof(xtype) * 8);
@@ -1164,9 +1126,7 @@ template <typename xtype, typename itype>
 struct xsl_rr_rr_mixin {
     typedef typename halfsize_trait<itype>::type htype;
 
-#ifdef __CUDACC__
-    __host__ __device__
-#endif
+GPU_FUNCTION
     static itype output(itype internal)
     {
         constexpr bitcount_t htypebits = bitcount_t(sizeof(htype) * 8);
@@ -1208,9 +1168,7 @@ struct xsl_rr_rr_mixin {
 template <typename xtype, typename itype>
 struct xsh_mixin {
 
-#ifdef __CUDACC__
-    __host__ __device__
-#endif
+GPU_FUNCTION
     static xtype output(itype internal)
     {
         constexpr bitcount_t xtypebits = bitcount_t(sizeof(xtype) * 8);
@@ -1235,9 +1193,7 @@ struct xsh_mixin {
 template <typename xtype, typename itype>
 struct xsl_mixin {
 
-#ifdef __CUDACC__
-    __host__ __device__
-#endif
+GPU_FUNCTION
     inline xtype output(itype internal)
     {
         constexpr bitcount_t xtypebits = bitcount_t(sizeof(xtype) * 8);
@@ -1364,9 +1320,7 @@ public:
         return baseclass::period_pow2() + table_size*extvalclass::period_pow2();
     }
 
-#ifdef __CUDACC__
-    __host__ __device__
-#endif
+GPU_FUNCTION
     PCG_ALWAYS_INLINE result_type operator()()
     {
         result_type rhs = get_extended_value();
