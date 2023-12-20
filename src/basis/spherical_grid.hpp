f4dac9a0115a852e2f961ed7e158c3e2beeb63e2
Xavier Andrade <xavier@tddft.org>
2023-12-19 20:34:47 -0800

    Call thrust remove_if from HIP.

--- src/basis/spherical_grid.hpp
+++ src/basis/spherical_grid.hpp
@@ -11,7 +11,7 @@
 
 #include <inq_config.h>
 
-#ifdef ENABLE_CUDA
+#ifdef ENABLE_GPU
 #include <thrust/remove.h>  // for thrust::remove_if
 #include <thrust/sort.h>
 #include <thrust/binary_search.h>
@@ -131,7 +131,7 @@ namespace basis {
 
 			{
 				CALI_CXX_MARK_SCOPE("spherical_grid::compact");				
-#ifdef ENABLE_CUDA
+#ifdef ENABLE_GPU
 				using thrust::remove_if;
 				auto it = remove_if(thrust::device, begin(points_), end(points_), [] GPU_LAMBDA (auto value){ return value.distance_ < 0.0;});
 				size_ = it - begin(points_);				
