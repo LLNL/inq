cc1faabfd2b3e57296a945a562cead3a9ac54aae
Xavier Andrade <xavier@tddft.org>
2024-01-15 02:14:34 -0800

    * Do not use the nofilter option in neon. * Restart the neon fourier pseudo calculation from the real space pseudo one.

--- tests/neon.cpp
+++ tests/neon.cpp
@@ -16,34 +16,37 @@ int main(int argc, char ** argv){
 	utils::match energy_match(2.0e-5);
 
 	systems::ions ions(systems::cell::cubic(15.0_b).finite());
-	ions.insert(input::species("Ne").nofilter(), {0.0_b, 0.0_b, 0.0_b});
+	ions.insert(input::species("Ne"), {0.0_b, 0.0_b, 0.0_b});
 	
 	//REAL SPACE PSEUDO
 	{
 		systems::electrons electrons(ions, options::electrons{}.extra_states(3).cutoff(30.0_Ha));
+		
 		ground_state::initial_guess(ions, electrons);
 		auto result = ground_state::calculate(ions, electrons, options::theory{}.non_interacting());
 		
-		energy_match.check("total energy",     result.energy.total()      , -61.765371991220);
-		energy_match.check("kinetic energy",   result.energy.kinetic()    ,  35.606535224997);
-		energy_match.check("eigenvalues",      result.energy.eigenvalues(), -61.765371991153);
-		energy_match.check("external energy",  result.energy.external()   , -79.444978433236);
-		energy_match.check("non-local energy", result.energy.nonlocal()   , -17.926928782914);
+		energy_match.check("total energy",     result.energy.total()      , -61.861045337100);
+		energy_match.check("kinetic energy",   result.energy.kinetic()    ,  35.765610219604);
+		energy_match.check("eigenvalues",      result.energy.eigenvalues(), -61.861045337100);
+		energy_match.check("external energy",  result.energy.external()   , -79.509954154661);
+		energy_match.check("non-local energy", result.energy.nonlocal()   , -18.116701402044);
 		energy_match.check("ion-ion energy",   result.energy.ion()        ,   0.000000000000);
 
+		electrons.save("neon_restart");
 	}
 
 	//FOURIER SPACE PSEUDO
 	{
 		systems::electrons electrons(ions, options::electrons{}.extra_states(3).cutoff(30.0_Ha).fourier_pseudo());
-		ground_state::initial_guess(ions, electrons);
+		electrons.load("neon_restart");
+
 		auto result = ground_state::calculate(ions, electrons, options::theory{}.non_interacting());
 		
-		energy_match.check("total energy",     result.energy.total()      , -61.765376105880);
-		energy_match.check("kinetic energy",   result.energy.kinetic()    ,  35.606511739929);
-		energy_match.check("eigenvalues",      result.energy.eigenvalues(), -61.765376105880);
-		energy_match.check("external energy",  result.energy.external()   , -79.444966794608);
-		energy_match.check("non-local energy", result.energy.nonlocal()   , -17.926921051201);
+		energy_match.check("total energy",     result.energy.total()      , -61.861056649453);
+		energy_match.check("kinetic energy",   result.energy.kinetic()    ,  35.765555684056);
+		energy_match.check("eigenvalues",      result.energy.eigenvalues(), -61.861056649453);
+		energy_match.check("external energy",  result.energy.external()   , -79.509918897873);
+		energy_match.check("non-local energy", result.energy.nonlocal()   , -18.116693435635);
 		energy_match.check("ion-ion energy",   result.energy.ion()        ,   0.000000000000);
 		
 	}
