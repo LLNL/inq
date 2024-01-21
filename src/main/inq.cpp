4739b375452535f2d85cfa9c3709948bbe9af849
Xavier Andrade <xavier@tddft.org>
2024-01-21 01:39:23 -0800

    Add an inq binary, for the moment it does nothing.

---
+++ src/main/inq.cpp
@@ -0,0 +1,17 @@
+/* -*- indent-tabs-mode: t -*- */
+
+// Copyright (C) 2019-2023 Lawrence Livermore National Security, LLC., Xavier Andrade, Alfredo A. Correa
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include <inq/inq.hpp>
+
+int main(int argc, char* argv[]) {
+
+	inq::input::environment::global(); //Initialize MPI 
+
+	fftw_cleanup();
+  return 0;
+}
