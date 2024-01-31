0280269863a1dbb0ff3345fcc7f3bd5fb0524014
Xavier Andrade <xavier@tddft.org>
2024-01-31 01:08:30 -0800

    Add a test for the command line interface.

---
+++ tests/h.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e #make the script fail if a command fails
+
+inq clear
+inq cell cubic 9.0 bohr finite
+inq ions add H 0.0 0.0 0.0 bohr
+inq electrons cutoff 25 hartree
+inq electrons polarized
+inq run ground_state
