ab1a6fc4166c3a916970d1ecbd6ca29e544b3bb9
Xavier Andrade <xavier@tddft.org>
2024-02-08 17:39:11 -0800

    Replaced graphene.cpp with graphene.sh.

---
+++ tests/graphene.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e #make the script fail if a command fails
+set -x #output commands to the terminal
+
+dcc=1.42
+a=`inq util calc "sqrt(3)*$dcc"`
+
+inq clear
+inq cell   $a 0 0  -$a/2 "(sqrt(3)/2)*$a" 0   0 0 5.2917721 Angstrom 2D
+
+inq ions insert C 0 0    0 Angstrom
+inq ions insert C 0 $dcc 0 Angstrom
+ 
+inq electrons extra states 2
+inq electrons spacing $a/15 Angstrom
+
+inq theory functional XC_GGA_X_RPBE XC_GGA_C_PBE
+
+inq ground-state tolerance 1e-8
+
+inq run ground-state
+
+inq util match `inq energy total`          -11.805691437798 3e-5
+inq util match `inq energy kinetic`          9.569622557902 3e-5
+inq util match `inq energy eigenvalues`     -3.508150981796 3e-5
+inq util match `inq energy hartree`        -11.111524722499 3e-5
+inq util match `inq energy external`        15.082403832279 3e-5
+inq util match `inq energy non-local`       -1.140029519851 3e-5
+inq util match `inq energy xc`              -4.392447120595 3e-5
+inq util match `inq energy nvxc`            -4.797098407128 3e-5
+inq util match `inq energy exact-exchange`   0.000000000000 3e-5
+inq util match `inq energy ion`            -19.813716465033 3e-5
