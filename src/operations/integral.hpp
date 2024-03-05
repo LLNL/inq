f03602b921bc06d234dcc46fffe9215e13037e36
Xavier Andrade <xavier@tddft.org>
2024-03-05 02:07:01 -0800

    GPU implementation of integral_product.

--- src/operations/integral.hpp
+++ src/operations/integral.hpp
@@ -75,9 +75,22 @@ auto integral_abs(basis::field<BasisType, ElementType> const & phi){
 }
 
 template <class BasisType, class ElementType1, class ElementType2>
-auto integral_product(basis::field<BasisType, ElementType1> const & phi1, basis::field<BasisType, ElementType2> const & phi2){
+auto integral_product(basis::field<BasisType, ElementType1> const & phi1, basis::field<BasisType, ElementType2> const & phi2) -> decltype(ElementType1{}*ElementType2{}) {
 	CALI_CXX_MARK_FUNCTION;
-	return integral(phi1, phi2, std::multiplies<>());
+
+	assert(phi1.basis() == phi2.basis());
+	
+	using type = decltype(ElementType1{}*ElementType2{});
+	gpu::array<type, 1> sum_array(phi1.basis().local_size());
+
+	gpu::run(phi1.basis().local_size(),
+					 [su = begin(sum_array), p1 = begin(phi1.linear()), p2 = begin(phi2.linear())] GPU_LAMBDA (auto ip) {
+						 su[ip] = p1[ip]*p2[ip];
+					 });
+
+	auto integral_value =  phi1.basis().volume_element()*sum(sum_array);
+	if(phi1.basis().comm().size() > 1) phi1.basis().comm().all_reduce_in_place_n(&integral_value, 1, std::plus<>{});
+	return integral_value;
 }
 
 template <class BasisType, class ElementType1, class ElementType2>
