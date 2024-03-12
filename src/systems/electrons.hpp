5cb2822a21067344a062d206a9ba5772a8786ba7
Xavier Andrade <xavier@tddft.org>
2024-03-11 17:29:42 -0700

    Initialize the atomic potential using the static version of gcutoff.

--- src/systems/electrons.hpp
+++ src/systems/electrons.hpp
@@ -141,7 +141,7 @@ public:
 		states_basis_comm_(states_basis_subcomm(full_comm_)),
 		states_basis_(ions.cell(), conf.spacing_value(), basis_subcomm(full_comm_)),
 		density_basis_(states_basis_), /* disable the fine density mesh for now density_basis_(states_basis_.refine(conf.density_factor(), basis_comm_)), */
-		atomic_pot_(ions.size(), ions.atoms(), states_basis_.gcutoff(), conf),
+		atomic_pot_(ions.size(), ions.atoms(), basis::real_space::gcutoff(ions.cell(), conf.spacing_value()), conf),
 		states_(conf.spin_val(), atomic_pot_.num_electrons() + conf.extra_electrons_val(), conf.extra_states_val(), conf.temperature_val(), kpts.size()),
 		spin_density_(density_basis_, states_.num_density_components()),
 		kpin_part_(kpts.size()*states_.num_spin_indices(), kpin_comm_)
