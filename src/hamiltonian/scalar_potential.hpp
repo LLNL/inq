ad484cbd1a4389a9ebf03bc095dc6035afa63734
Xavier Andrade <xavier@tddft.org>
2024-04-10 14:24:59 -0700

    Implement the scalar potential application for spinors.

--- src/hamiltonian/scalar_potential.hpp
+++ src/hamiltonian/scalar_potential.hpp
@@ -22,15 +22,28 @@ namespace hamiltonian {
 
 template <class PotentialType, class ShiftType>
 void scalar_potential_add(basis::field_set<basis::real_space, PotentialType> const & potential, int const index, ShiftType shift, states::orbital_set<basis::real_space, complex> const & phi, states::orbital_set<basis::real_space, complex> & vphi) {
-
 	CALI_CXX_MARK_FUNCTION;
   
   assert(potential.basis() == phi.basis());
-  
-  gpu::run(phi.local_set_size(), phi.basis().local_size(),
-           [pot = begin(potential.matrix()), it_vphi = begin(vphi.matrix()), it_phi = begin(phi.matrix()), shift, index] GPU_LAMBDA (auto ist, auto ip){
-             it_vphi[ip][ist] += (pot[ip][index] + shift)*it_phi[ip][ist];
-           });
+
+	if(not phi.spinors()){
+		
+		gpu::run(phi.local_set_size(), phi.basis().local_size(),
+						 [pot = begin(potential.matrix()), vph = begin(vphi.matrix()), ph = begin(phi.matrix()), shift, index] GPU_LAMBDA (auto ist, auto ip){
+							 vph[ip][ist] += (pot[ip][index] + shift)*ph[ip][ist];
+						 });
+	} else {
+
+		assert(potential.local_set_size() == 4);
+		
+		gpu::run(phi.local_spinor_set_size(), phi.basis().local_size(),
+						 [pot = begin(potential.matrix()), vph = begin(vphi.spinor_array()), ph = begin(phi.spinor_array()), shift] GPU_LAMBDA (auto ist, auto ip){
+							 auto offdiag = pot[ip][2] + complex{0.0, 1.0}*pot[ip][3];
+							 vph[ip][0][ist] += (pot[ip][0] + shift)*ph[ip][0][ist] + offdiag*ph[ip][1][ist];
+							 vph[ip][1][ist] += (pot[ip][1] + shift)*ph[ip][1][ist] + conj(offdiag)*ph[ip][0][ist];
+						 });
+	}
+	
 }
 
 template <class PotentialType, class ShiftType>
@@ -41,12 +54,26 @@ states::orbital_set<basis::real_space, complex> scalar_potential(basis::field_se
   states::orbital_set<basis::real_space, complex> vphi(phi.skeleton());
   
   assert(potential.basis() == phi.basis());
-	
-  gpu::run(phi.local_set_size(), phi.basis().local_size(),
-           [pot = begin(potential.matrix()), it_vphi = begin(vphi.matrix()), it_phi = begin(phi.matrix()), shift, index] GPU_LAMBDA (auto ist, auto ip){
-             it_vphi[ip][ist] = (pot[ip][index] + shift)*it_phi[ip][ist];
-           });
 
+	if(not phi.spinors()){
+
+		gpu::run(phi.local_set_size(), phi.basis().local_size(),
+						 [pot = begin(potential.matrix()), vph = begin(vphi.matrix()), ph = begin(phi.matrix()), shift, index] GPU_LAMBDA (auto ist, auto ip){
+							 vph[ip][ist] = (pot[ip][index] + shift)*ph[ip][ist];
+						 });
+	} else {
+		
+		assert(potential.local_set_size() == 4);
+		
+		gpu::run(phi.local_spinor_set_size(), phi.basis().local_size(),
+						 [pot = begin(potential.matrix()), vph = begin(vphi.spinor_array()), ph = begin(phi.spinor_array()), shift] GPU_LAMBDA (auto ist, auto ip){
+							 auto offdiag = pot[ip][2] + complex{0.0, 1.0}*pot[ip][3];
+							 vph[ip][0][ist] = (pot[ip][0] + shift)*ph[ip][0][ist] + offdiag*ph[ip][1][ist];
+							 vph[ip][1][ist] = (pot[ip][1] + shift)*ph[ip][1][ist] + conj(offdiag)*ph[ip][0][ist];
+						 });
+
+	}
+		
   return vphi;
   
 }
