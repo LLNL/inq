82a0cd1f8683a314ece15a289dce95708afbd019
Xavier Andrade <xavier@tddft.org>
2024-04-13 23:26:01 -0700

    Insert atoms to the GaAs example.

--- tests/gaas.py
+++ tests/gaas.py
@@ -3,6 +3,10 @@ import pinq
 alat = 565.315
 
 pinq.clear()
+
 pinq.cell.lattice([0.0, 1.0/2.0, 1.0/2.0], [1.0/2.0, 0.0, 1.0/2.0], [1.0/2.0, 1.0/2.0, 0.0], alat, "pm")
 pinq.cell.show()
 
+pinq.ions.insert_fractional("Ga", [0.00, 0.00, 0.00]);
+pinq.ions.insert_fractional("As", [0.25, 0.25, 0.25]);
+pinq.ions.show()
