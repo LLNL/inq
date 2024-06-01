a26dee91d13afba6e4a7fdd714e4650d37da03c5
Xavier Andrade <xavier@llnl.gov>
2024-06-01 17:01:59 +0800

    Updated the results for the BN test.

--- tests/bn.sh
+++ tests/bn.sh
@@ -18,14 +18,13 @@ inq ground-state tolerance 1e-8
 
 inq run ground-state
 
-inq util match `inq results ground-state energy total`           -13.415882369125 3e-5
-inq util match `inq results ground-state energy kinetic`           9.561946756363 3e-5
-inq util match `inq results ground-state energy eigenvalues`      -0.946744600841 3e-5
-inq util match `inq results ground-state energy hartree`           1.768191600618 3e-5
-inq util match `inq results ground-state energy external`         -7.925687586347 3e-5
-inq util match `inq results ground-state energy non-local`        -1.120110161748 3e-5
-inq util match `inq results ground-state energy xc`               -4.410160558972 3e-5
-inq util match `inq results ground-state energy nvxc`             -4.999276810345 3e-5
+inq util match `inq results ground-state energy total`           -13.415121196063 3e-5
+inq util match `inq results ground-state energy kinetic`           9.561233691561 3e-5
+inq util match `inq results ground-state energy eigenvalues`      -0.946326812486 3e-5
+inq util match `inq results ground-state energy hartree`           1.767819938212 3e-5
+inq util match `inq results ground-state energy external`         -7.924084911288 3e-5
+inq util match `inq results ground-state energy non-local`        -1.120000193499 3e-5
+inq util match `inq results ground-state energy xc`               -4.410027302009 3e-5
+inq util match `inq results ground-state energy nvxc`             -4.999115275683 3e-5
 inq util match `inq results ground-state energy exact-exchange`    0.000000000000 3e-5
 inq util match `inq results ground-state energy ion`             -11.290062419039 3e-5
-
