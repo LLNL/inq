e50fd0aad8d7586ce03a476a4b6d3623c7c4a60c
Xavier Andrade <xavier@tddft.org>
2024-06-25 13:59:38 -0700

    Indent the history.hpp file with tabs.

--- src/interface/history.hpp
+++ src/interface/history.hpp
@@ -36,25 +36,25 @@ These are the available subcommands:
 
 - `history`
 
-  Prints a full list of the inq commands that have been executed in the
-  current directory.
+	Prints a full list of the inq commands that have been executed in the
+	current directory.
 
-  Shell example: `inq history`
+	Shell example: `inq history`
 
 - `history clear`
 
-  Clears the list of saved commands.
+	Clears the list of saved commands.
 
-  Shell example: `inq history clear`
+	Shell example: `inq history clear`
 
 - `history script`
 
-  Prints a bash script containing the list of commands that reproduce
-  the calculation in the current directory. The recommended script
-  headers are included. Any command issues before `inq clear` will not
-  be included.
+	Prints a bash script containing the list of commands that reproduce
+	the calculation in the current directory. The recommended script
+	headers are included. Any command issues before `inq clear` will not
+	be included.
 
-  Shell example: `inq history script > my_calculation.sh`
+	Shell example: `inq history script > my_calculation.sh`
 
 
 )"""";
@@ -63,50 +63,50 @@ These are the available subcommands:
 	template <typename ArgsType>
 	void command(ArgsType const & args, bool quiet) const {
 
-    if(args.size() == 0) {
-      std::ifstream hist_file(".inq_history");
-      if(hist_file.is_open()) std::cout << hist_file.rdbuf();
-      actions::normal_exit();
-    }                   
+		if(args.size() == 0) {
+			std::ifstream hist_file(".inq_history");
+			if(hist_file.is_open()) std::cout << hist_file.rdbuf();
+			actions::normal_exit();
+		}										
 
 		if(args.size() == 1 and args[0] == "clear") {
-      std::ofstream hist_file(".inq_history");
+			std::ofstream hist_file(".inq_history");
 			actions::normal_exit();
 		}
 
-    if(args.size() == 1 and args[0] == "script") {
+		if(args.size() == 1 and args[0] == "script") {
 
 			auto starts_with = [](auto str, auto substr) {
 				return utils::lowercase(str).rfind(substr, 0) == 0;
 			};
 			
-      std::cout << "#!/bin/bash\n\n"
-                << "set -e #make the script fail if a command fails\n"
-                << "set -x #output commands to the terminal\n\n"
-                << "inq clear\n";
+			std::cout << "#!/bin/bash\n\n"
+								<< "set -e #make the script fail if a command fails\n"
+								<< "set -x #output commands to the terminal\n\n"
+								<< "inq clear\n";
 
-      std::ifstream hist_file(".inq_history");
+			std::ifstream hist_file(".inq_history");
 
-      std::list<std::string> file_lines;
-      while(hist_file) {
-        std::string line;
-        std::getline(hist_file, line);
+			std::list<std::string> file_lines;
+			while(hist_file) {
+				std::string line;
+				std::getline(hist_file, line);
 
-        if(starts_with(line, "inq history")) continue;
+				if(starts_with(line, "inq history")) continue;
 				
-        file_lines.push_back(line);
-      }
+				file_lines.push_back(line);
+			}
 
 			auto rit = file_lines.rbegin();
 			for(; rit != file_lines.rend(); ++rit){
 				if(starts_with(*rit, "inq clear") or starts_with(*rit, "inq clean")) break;
 			}
 			
-      for(auto it = rit.base(); it != file_lines.end(); ++it) std::cout << *it << '\n';
-      
-      actions::normal_exit();
-    }
-    
+			for(auto it = rit.base(); it != file_lines.end(); ++it) std::cout << *it << '\n';
+			
+			actions::normal_exit();
+		}
+		
 		actions::error(input::environment::global().comm(), "Invalid syntax in the 'history' command");
 	}
 	
