57d9bfdcbc3b58e611d28fe93f734cacca7e23bf
Xavier Andrade <xavier@tddft.org>
2024-07-09 10:47:38 -0700

    Print the list of species at the beginning of a run.

--- src/systems/electrons.hpp
+++ src/systems/electrons.hpp
@@ -262,7 +262,8 @@ public:
 			
 
 		if(logger()){
-			logger()->info("constructed with cell {}", ions.cell());
+			logger()->info("constructed with cell\n {}", ions.cell());
+			logger()->info("constructed with species:\n {}", ions.species_list());
 			logger()->info("constructed with geometry:\n {}", ions);
 			if(ions.size() > 0) logger()->info("system symmetries: " + ions.symmetry_string());
 			logger()->info("constructed with Brillouin zone sampling {}", brillouin_zone_);
