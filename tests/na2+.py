ff47c83cbac7867bd4b3b3737774d68c59cc10a6
Xavier Andrade <xavier@tddft.org>
2024-08-18 23:11:41 -0700

    Add tests for the energy resulst in Na2+

--- tests/na2+.py
+++ tests/na2+.py
@@ -43,3 +43,18 @@ pinq.real_time.observables.dipole()
 pinq.perturbations.kick([0, 0, 0.01])
 
 pinq.run.real_time()
+
+energy = pinq.results.real_time.total_energy()
+
+assert pinq.util.match(energy[   0], -0.6438225831700184, 3e-5)
+assert pinq.util.match(energy[  10], -0.6438225831586412, 3e-5)
+assert pinq.util.match(energy[ 300], -0.6438225835346540, 3e-5)
+assert pinq.util.match(energy[ 444], -0.6438225837767503, 3e-5)
+assert pinq.util.match(energy[1000], -0.6438225844307714, 3e-5)
+assert pinq.util.match(energy[1663], -0.6438225852107715, 3e-5)
+assert pinq.util.match(energy[2000], -0.6438225854501384, 3e-5)
+assert pinq.util.match(energy[2101], -0.6438225856304741, 3e-5)
+assert pinq.util.match(energy[2748], -0.6438225861809898, 3e-5)
+assert pinq.util.match(energy[2937], -0.6438225863718710, 3e-5)
+assert pinq.util.match(energy[3000], -0.6438225864372554, 3e-5)
+
