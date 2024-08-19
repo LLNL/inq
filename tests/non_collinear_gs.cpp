5102dbb735d565e729fa02a501d0fde80552d86f
Jacopo Simoni <simonij@tcd.ie>
2024-08-18 21:56:48 -0500

    final GS calculations tests

--- tests/non_collinear_gs.cpp
+++ tests/non_collinear_gs.cpp
@@ -11,15 +11,16 @@ auto compute_GS(EnvType const & env, systems::ions const & ions) {
 
     auto electrons = systems::electrons(env.par(), ions, options::electrons{}.cutoff(30.0_Ha).extra_states(2).spin_polarized());
     ground_state::initial_guess(ions, electrons);
-    auto result_col = ground_state::calculate(ions, electrons, options::theory{}.lda(), inq::options::ground_state{}.steepest_descent().energy_tolerance(1.e-8_Ha).max_steps(100));
+    auto result_col = ground_state::calculate(ions, electrons, options::theory{}.lda(), inq::options::ground_state{}.steepest_descent().energy_tolerance(1.e-8_Ha).max_steps(1000));
     auto mag = observables::total_magnetization(electrons.spin_density());
 
     auto electrons_nc = systems::electrons(env.par(), ions, options::electrons{}.cutoff(30.0_Ha).extra_states(2).spin_non_collinear());
     ground_state::initial_guess(ions, electrons_nc);
-    auto result_nc = ground_state::calculate(ions, electrons_nc, options::theory{}.lda(), inq::options::ground_state{}.steepest_descent().energy_tolerance(1.e-8_Ha).max_steps(100));
+    auto result_nc = ground_state::calculate(ions, electrons_nc, options::theory{}.lda(), inq::options::ground_state{}.steepest_descent().energy_tolerance(1.e-8_Ha).max_steps(1000).mixing(0.1));
     auto mag_nc = observables::total_magnetization(electrons_nc.spin_density());
 
-    match.check("total magnetization",        mag.length(),      mag_nc.length());
+    match.check("total energy",               result_col.energy.total(),   result_nc.energy.total());
+    match.check("total magnetization",        mag.length(),                mag_nc.length());
 }
 
 
@@ -35,4 +36,5 @@ int main (int argc, char ** argv) {
     ions.insert("O", {0.0_b, 0.0_b, d/2});
     ions.insert("O", {0.0_b, 0.0_b,-d/2});
     compute_GS(env, ions);
+    
 }
