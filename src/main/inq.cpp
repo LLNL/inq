cdd2f723bf0f8510e05c45dfcc8110b59c03fa25
Xavier Andrade <xavier@tddft.org>
2024-09-10 14:51:56 -0700

    Add fraction as an alias to fractional.

--- src/main/inq.cpp
+++ src/main/inq.cpp
@@ -23,6 +23,7 @@ int main(int argc, char* argv[]) {
 		{ "extraelectrons"s,           "extra-electrons"s          },
 		{ "extrastates"s,              "extra-states"s             },
 		{ "filename"s,                 "file"s                     },
+		{ "fraction"s,                 "fractional"s               },
 		{ "freq"s,                     "frequency"s                },
 		{ "functionals"s,              "functional"s               },
 		{	"groundstate"s,              "ground-state"s             },
