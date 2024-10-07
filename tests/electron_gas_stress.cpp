7cb683c17759b858073d2a45a5875e6f81f04de0
Alfredo Correa <alfredo.correa@gmail.com>
2024-10-07 15:55:49 -0700

    convert energy calculation into a function

--- tests/electron_gas_stress.cpp
+++ tests/electron_gas_stress.cpp
@@ -26,7 +26,23 @@
 
 #include <input/environment.hpp>
 
-int main(int argc, char ** argv){
+template<class LatticeParameters>
+auto total_energy(inq::input::environment& env, LatticeParameters lat) {
+	using namespace inq;
+	using namespace inq::magnitude;
+
+    auto const box = systems::cell::cubic(lat);
+
+    systems::ions const ions(box);
+
+    systems::electrons electrons(env.par(), ions, options::electrons{}.cutoff(30.0_Ha).temperature(300.0_K).extra_electrons(14.0).extra_states(2), input::kpoints::grid({1, 1, 1}, false));
+
+    ground_state::initial_guess(ions, electrons);
+
+    return ground_state::calculate(ions, electrons, options::theory{}.lda(), inq::options::ground_state{}.energy_tolerance(1e-9_Ha));
+}
+
+int main(int argc, char** argv){
 
 	using namespace inq;
 	using namespace inq::magnitude;
@@ -35,20 +51,12 @@ int main(int argc, char ** argv){
 	
 	utils::match energy_match(1.0e-5);
 
-	{
-		auto box = systems::cell::cubic(10.0_b);
-		
-		systems::ions ions(box);
+    auto const result = total_energy(env, 10.0_b);
 
-        systems::electrons electrons(env.par(), ions, options::electrons{}.cutoff(30.0_Ha).temperature(300.0_K).extra_electrons(14.0).extra_states(2), input::kpoints::grid({1, 1, 1}, false));
-		
-		ground_state::initial_guess(ions, electrons);
+    energy_match.check("total energy",        result.energy.total()    , -0.567967321401);
+    energy_match.check("kinetic energy",      result.energy.kinetic()  ,  2.485678165423);
 
-        auto result = ground_state::calculate(ions, electrons, options::theory{}.lda(), inq::options::ground_state{}.energy_tolerance(1e-9_Ha));
-		
-		energy_match.check("total energy",        result.energy.total()    , -0.567967321401);
-		energy_match.check("kinetic energy",      result.energy.kinetic()  ,  2.485678165423);
-//		energy_match.check("hartree",             result.energy.hartree    ,  0.000000732036);	
-//		energy_match.check("XC energy",           result.energy.xc         , -3.053646218860);
-	}
+    // energy_match.check("hartree",             result.energy.hartree    ,  0.000000732036);	
+    // energy_match.check("XC energy",           result.energy.xc         , -3.053646218860);
+
 }
