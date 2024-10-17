bb0fa4d96b629ed9cbd1c449fe48eb046ec612e6
Jacopo Simoni <simonij@tcd.ie>
2024-10-17 04:38:11 -0500

    modified zeeman_coupling - only private member is num_spin_components - the B field is not stored as a member

--- src/hamiltonian/zeeman_coupling.hpp
+++ src/hamiltonian/zeeman_coupling.hpp
@@ -1,7 +1,7 @@
 /* -*- indent-tabs-mode: t -*- */
 
-#ifndef ZEEMAN_COUPL_HPP
-#define ZEEMAN_COUPL_HPP
+#ifndef ZEEMAN_COUPLING_HPP
+#define ZEEMAN_COUPLING_HPP
 
 namespace inq {
 namespace hamiltonian {
@@ -10,18 +10,15 @@ class zeeman_coupling {
 
 private:
 
-    vector3<double> B_;
     int spin_components_;
 
 public:
 
-    zeeman_coupling(int const spin_components, vector3<double> const & B):
-        spin_components_(spin_components),
-        B_(B)
+    zeeman_coupling(int const spin_components):
+        spin_components_(spin_components)
     {
         assert(spin_components_ > 1);
-        std::cout << "SPIN COMPONENTS : " << spin_components_ << std::endl;
-        std::cout << B_ << std::endl;   
+        std::cout << "SPIN COMPONENTS : " << spin_components_ << std::endl;   
     }
 
 };
@@ -30,15 +27,26 @@ public:
 }
 #endif
 
-#ifdef INQ_HAMILTONIAN_ZEEMAN_COUPL_UNIT_TEST
-#undef INQ_HAMILTONIAN_ZEEMAN_COUPL_UNIT_TEST
+#ifdef INQ_HAMILTONIAN_ZEEMAN_COUPLING_UNIT_TEST
+#undef INQ_HAMILTONIAN_ZEEMAN_COUPLING_UNIT_TEST
+
+#include <perturbations/magnetic.hpp>
 
 TEST_CASE(INQ_TEST_FILE, INQ_TEST_TAG) {
 
+    using namespace inq;
+    using namespace inq::magnitude;
+
     parallel::communicator comm{boost::mpi3::environment::get_world_instance()};
 
-    SECTION("Spin polarized vz calculation") {
+    SECTION("Spin polarized zeeman calculation") {
         auto par = input::parallelization(comm);
+        auto ions = systems::ions(systems::cell::cubic(10.0_b));
+        ions.insert("H", {0.0_b, 0.0_b, 0.0_b});
+        auto electrons = systems::electrons(par, ions, options::electrons{}.cutoff(30.0_Ha).extra_states(2).spin_polarized());
+        ground_state::initial_guess(ions, electrons);
+        perturbations::magnetic B{{0.0, 0.0, -1.0}};
+        auto result = ground_state::calculate(ions, electrons, options::theory{}.lda(), inq::options::ground_state{}.steepest_descent().energy_tolerance(1.e-8_Ha).max_steps(1000).mixing(0.1), B);
     }
 }
 #endif
