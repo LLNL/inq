cd20b440a5cb25664693752e7e8ba8c19f26c8af
Jacopo Simoni <simonij@tcd.ie>
2024-10-17 21:22:48 -0500

    ext. magnetic field for O2

--- tests/ext_mag_field.cpp
+++ tests/ext_mag_field.cpp
@@ -16,11 +16,12 @@ int main (int argc, char ** argv){
     ions.insert("O", {0.0_b, 0.0_b, d/2});
     ions.insert("O", {0.0_b, 0.0_b,-d/2});
 
-    auto electrons = systems::electrons(env.par(), ions, options::electrons{}.cutoff(30.0_Ha).extra_states(2).spin_polarized());
+    auto electrons = systems::electrons(env.par(), ions, options::electrons{}.cutoff(30.0_Ha).extra_states(10).spin_polarized());
     ground_state::initial_guess(ions, electrons);
-    perturbations::magnetic B{{0.0, 0.0, 1.0}};
+    perturbations::magnetic B{{0.0, 0.0, 0.4}};
     auto result = ground_state::calculate(ions, electrons, options::theory{}.lda(), inq::options::ground_state{}.steepest_descent().energy_tolerance(1.e-8_Ha).max_steps(1000).mixing(0.1), B);
     auto mag = observables::total_magnetization(electrons.spin_density());
+    std::cout << mag << std::endl;
 
     match.check("magnetization direction",        mag/mag.length(), {0.0, 0.0, 1.0});
     return match.fail();
