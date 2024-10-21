057a8c4a4bc1a822008e8a4d53ad85c16cdf297e
jacopos86 <simonij@tcd.ie>
2024-10-21 09:46:44 -0500

    included the non collinear part of the test and the collinear with opposite B field

--- tests/ext_mag_field.cpp
+++ tests/ext_mag_field.cpp
@@ -22,7 +22,23 @@ int main (int argc, char ** argv){
     auto result = ground_state::calculate(ions, electrons, options::theory{}.lda(), inq::options::ground_state{}.steepest_descent().energy_tolerance(1.e-8_Ha).max_steps(1000).mixing(0.1), B);
     auto mag = observables::total_magnetization(electrons.spin_density());
     std::cout << mag << std::endl;
-
     match.check("magnetization direction",        mag/mag.length(), {0.0, 0.0, 1.0});
+
+    perturbations::magnetic B2{{0.0, 0.0, -0.4}};
+    result = ground_state::calculate(ions, electrons, options::theory{}.lda(), inq::options::ground_state{}.steepest_descent().energy_tolerance(1.e-8_Ha).max_steps(1000).mixing(0.1), B2);
+    auto mag2 = observables::total_magnetization(electrons.spin_density());
+    std::cout << mag2 << std::endl;
+    match.check("magnetization direction",        mag2/mag2.length(), {0.0, 0.0, -1.0});
+
+    perturbations::magnetic B3{{0.001, 0.0, -0.4}};
+    electrons = systems::electrons(env.par(), ions, options::electrons{}.cutoff(30.0_Ha).extra_states(10).spin_non_collinear());
+    ground_state::initial_guess(ions, electrons);
+    result = ground_state::calculate(ions, electrons, options::theory{}.lda(), inq::options::ground_state{}.steepest_descent().energy_tolerance(1.e-8_Ha).max_steps(1000).mixing(0.01), B3);
+    auto mag3 = observables::total_magnetization(electrons.spin_density());
+    std::cout << mag3 << std::endl;
+    vector3 e_v = {0.001, 0.0, -0.4};
+    e_v = e_v/sqrt(norm(e_v));
+    match.check("magnetization direction",        mag3/mag3.length(), e_v);
+
     return match.fail();
 }
