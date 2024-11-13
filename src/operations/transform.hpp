c39220e7f4fd889b01c5e9615a1123262f031aba
Alfredo Correa <alfredo.correa@gmail.com>
2024-11-12 16:41:52 -0800

    more std::get

--- src/operations/transform.hpp
+++ src/operations/transform.hpp
@@ -58,7 +58,9 @@ void to_fourier_array(basis::real_space const & real_basis, basis::fourier_space
 
 	CALI_CXX_MARK_FUNCTION;
 
-	assert(std::get<3>(sizes(array_rs)) == std::get<3>(sizes(array_fs)));
+	using std::get;
+
+	assert(get<3>(sizes(array_rs)) == get<3>(sizes(array_fs)));
 	
 	CALI_MARK_BEGIN("heffte_initialization");
  
@@ -171,7 +173,9 @@ void to_fourier_array(basis::real_space const & real_basis, basis::fourier_space
 		}
 		CALI_MARK_END("fft_forward_transpose");
 
-		assert(std::get<4>(sizes(buffer)) == last_dim);
+		using std::get;
+
+		assert(get<4>(sizes(buffer)) == last_dim);
 		
 		tmp.clear();
 
