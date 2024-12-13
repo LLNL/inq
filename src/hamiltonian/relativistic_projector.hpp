e905c034f6b805c8b8f75d645f670336046d70d0
Xavier Andrade <xavier@tddft.org>
2024-12-13 08:33:17 -0800

    Pass the template type to sharmonic_complex.

--- src/hamiltonian/relativistic_projector.hpp
+++ src/hamiltonian/relativistic_projector.hpp
@@ -79,12 +79,12 @@ public: // for CUDA
 									metric = basis.cell().metric()] GPU_LAMBDA (auto ipoint) {
 
 									 if(abs(ml0) <= ll) {
-										 bet[iproj_lm][ipoint][0] = cc0*spline(sph.distance(ipoint))*pseudo::math::sharmonic_complex(ll, ml0, metric.to_cartesian(sph.point_pos(ipoint)));
+										 bet[iproj_lm][ipoint][0] = cc0*spline(sph.distance(ipoint))*pseudo::math::sharmonic_complex<complex>(ll, ml0, metric.to_cartesian(sph.point_pos(ipoint)));
 									 } else {
 										 bet[iproj_lm][ipoint][0] = 0.0;
 									 }
 									 if(abs(ml1) <= ll) {									 
-										 bet[iproj_lm][ipoint][1] = cc1*spline(sph.distance(ipoint))*pseudo::math::sharmonic_complex(ll, ml1, metric.to_cartesian(sph.point_pos(ipoint)));
+										 bet[iproj_lm][ipoint][1] = cc1*spline(sph.distance(ipoint))*pseudo::math::sharmonic_complex<complex>(ll, ml1, metric.to_cartesian(sph.point_pos(ipoint)));
 									 } else {
 										 bet[iproj_lm][ipoint][1] = 0.0;
 									 }
