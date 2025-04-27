d500d37ccf9d6cd7ffcec325a98c3d235464973d
Jacopo Simoni <simonij@tcd.ie>
2025-04-26 22:24:48 -0500

    magnetic susceptibility for Rb atom

--- tests/mag_suscept.cpp
+++ tests/mag_suscept.cpp
@@ -20,35 +20,38 @@ int main(int argc, char ** argv){
     // metallic Na
     // bcc : a = 0.428 nm
 
-    auto a = 4.28_A;
+    auto a = 5.585_A;
     auto cell = systems::cell::lattice({-a/2.0, a/2.0, a/2.0}, {a/2.0, -a/2.0, a/2.0}, {a/2.0, a/2.0, -a/2.0});
     auto ions = systems::ions(cell);
-    ions.insert_fractional("Na", {0.0, 0.0, 0.0});
-    ions.insert_fractional("Na", {0.5, 0.5, 0.5});
+    ions.insert_fractional("Rb", {0.0, 0.0, 0.0});
 	
-	systems::electrons electrons(env.par(), ions, options::electrons{}.cutoff(30.0_Ha).spin_non_collinear().extra_states(5), input::kpoints::grid({2, 2, 2}));
+	systems::electrons electrons(env.par(), ions, options::electrons{}.cutoff(40.0_Ha).spin_non_collinear().extra_states(15).temperature(300.0_K), input::kpoints::grid({8, 8, 8}));
 	ground_state::initial_guess(ions, electrons);
 
     auto scf_options = options::ground_state{}.energy_tolerance(1.0e-9_Ha).max_steps(500).broyden_mixing();
+    std::vector<double> mx;
+    std::vector<double> my;
     std::vector<double> mz;
     std::vector<double> bz;
 
     for (auto i=0; i<10; i++) {
-        vector3 bvec = {0.0_amu, 0.0_amu, 0.1_amu};
+        vector3 bvec = {0.0_beV, 0.0_beV, 0.1_beV};
         bvec = bvec * double(i);
         std::cout << bvec[0] << " " << bvec[1] << " " << bvec[2] << std::endl;
         perturbations::magnetic B{bvec};
         auto result = ground_state::calculate(ions, electrons, options::theory{}.lda(), scf_options, B);
         auto mag = observables::total_magnetization(electrons.spin_density());
-        mz.push_back(mag[2]/cell.volume());                   // bohr^-3
-        bz.push_back(bvec[2].in_atomic_units()*2.3505e+05);   // tesla
+        mz.push_back(mag[2]);
+        mx.push_back(mag[0]);
+        my.push_back(mag[1]);
+        bz.push_back(i*0.1);                  // eV
     }
 
     if (electrons.kpin_states_comm().rank() == 0) {
-        std::ofstream outfile("chi_al.txt");
+        std::ofstream outfile("Rb_bcc.txt");
         if (outfile.is_open()) {
             for (auto i=0; i<mz.size(); i++){
-                outfile << bz[i] << "          " << mz[i] << std::endl;
+                outfile << bz[i] << "          " << mx[i] << "          " << my[i] << "          " << mz[i] << std::endl;
             }
             outfile.close();
             std::cout << "File written successfully" << std::endl;
