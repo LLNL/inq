eac1027f7d3f28795d62e9de7f43e22f05643016
Xavier Andrade <xavier@llnl.gov>
2025-08-23 12:01:08 -0700

    Define a type for the stress.

--- src/observables/forces_stress.hpp
+++ src/observables/forces_stress.hpp
@@ -20,8 +20,11 @@ namespace inq {
 namespace observables {
 
 struct forces_stress {
+
+	using stress_type = vector3<vector3<double>>;
+	
 	gpu::array<vector3<double>, 1> forces;
-	vector3<vector3<double>> stress;
+	stress_type stress;
 
 	forces_stress() = default;
 
@@ -61,7 +64,7 @@ private:
 	
 	template <typename Stress1D>
 	auto tensor(Stress1D const & stress1d) {
-		vector3<vector3<double>> stress;
+		stress_type stress;
 
 		for(auto index = 0; index < 6; index++) {
 			int alpha, beta;
@@ -76,7 +79,7 @@ private:
 	////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
 	template <typename GPhi, typename Occupations>
-	vector3<vector3<double>> stress_kinetic(GPhi const & gphi, Occupations const & occupations) {
+	stress_type stress_kinetic(GPhi const & gphi, Occupations const & occupations) {
 
 		auto stress1d = gpu::run(6, gpu::reduce(gphi.local_set_size()), gpu::reduce(gphi.basis().local_size()), 0.0,
 														 [metric = gphi.basis().cell().metric(), gph = begin(gphi.matrix()), occ = begin(occupations)] GPU_LAMBDA (auto index, auto ist, auto ip) {
@@ -94,7 +97,7 @@ private:
 	////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
 	template <typename Density>
-	vector3<vector3<double>> stress_electrostatic(Density const & density) {
+	stress_type stress_electrostatic(Density const & density) {
 
 		auto potential = solvers::poisson::solve(density);
 		auto efield = operations::gradient(potential);
@@ -123,12 +126,7 @@ private:
 		
 		CALI_CXX_MARK_FUNCTION;
 
-		// SET THE STRESS TO ZERO
-		for(auto alpha = 0; alpha < 3; alpha++){
-			for(auto beta = 0; beta < 3; beta++){
-				stress[alpha][beta] = 0.0;
-			}
-		}
+		stress = zero<stress_type>();
 		
 		basis::field<basis::real_space, vector3<double, covariant>> gdensity(electrons.density_basis());
 		gdensity.fill(vector3<double, covariant>{0.0, 0.0, 0.0});
@@ -141,7 +139,7 @@ private:
 			auto gphi = operations::gradient(phi, /* factor = */ 1.0, /*shift = */ phi.kpoint() + ham.uniform_vector_potential());
 			observables::density::calculate_gradient_add(electrons.occupations()[iphi], phi, gphi, gdensity);
 
-			vector3<vector3<double>> stress_non_local;
+			stress_type stress_non_local;
 			ham.projectors_all().force_stress(phi, gphi, electrons.occupations()[iphi], phi.kpoint() + ham.uniform_vector_potential(), forces_non_local, stress_non_local);
 
 			stress += stress_non_local;
