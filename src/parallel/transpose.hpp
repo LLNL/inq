e9505a963509eb8eb634fff95b8e620d1fb72d96
Xavier Andrade <xavier@llnl.gov>
2025-09-05 01:16:47 -0700

    Add the parallel::transpose function that transposes a distributed matrix.

---
+++ src/parallel/transpose.hpp
@@ -0,0 +1,143 @@
+/* -*- indent-tabs-mode: t -*- */
+
+#ifndef INQ__PARALLEL__TRANSPOSE
+#define INQ__PARALLEL__TRANSPOSE
+
+// Copyright (C) 2019-2023 Lawrence Livermore National Security, LLC., Xavier Andrade, Alfredo A. Correa
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include <inq_config.h>
+
+#include <cstdlib>
+
+#include <gpu/array.hpp>
+#include <utils/raw_pointer_cast.hpp>
+
+#include <inq_config.h>
+#include <mpi.h>
+#include <parallel/communicator.hpp>
+#include <gpu/run.hpp>
+
+namespace inq {
+namespace parallel {
+
+template <typename PartX, typename PartY, typename Matrix>
+void transpose(parallel::communicator & comm, PartX const & partx, PartY const & party, Matrix & matrix){
+	CALI_CXX_MARK_FUNCTION;
+
+	using type = typename Matrix::element_type;
+
+	auto bsize = partx.max_local_size()*party.max_local_size();
+	
+	Matrix buffer({comm.size(), bsize});
+	
+	gpu::run(party.max_local_size(), partx.max_local_size(), comm.size(),
+					 [mat = begin(matrix), buf = begin(buffer), partx, party] GPU_LAMBDA (auto iy, auto ix, auto iproc) { 
+						 if(ix < partx.local_size(iproc) and iy < party.local_size()) buf[iproc][ix + iy*partx.max_local_size()] = mat[partx.start(iproc) + ix][iy];
+					 });
+
+	parallel::alltoall(buffer, comm);
+
+	matrix.reextent({party.size(), partx.local_size()});
+	
+	gpu::run(partx.max_local_size(), party.max_local_size(), comm.size(),
+					 [mat = begin(matrix), buf = begin(buffer), partx, party] GPU_LAMBDA (auto ix, auto iy, auto iproc) { 
+						 if(iy < party.local_size(iproc) and ix < party.local_size()) mat[party.start(iproc) + iy][ix] = buf[iproc][ix + iy*partx.max_local_size()];
+					 });
+
+}
+
+
+}
+}
+#endif
+
+#ifdef INQ_PARALLEL_TRANSPOSE_UNIT_TEST
+#undef INQ_PARALLEL_TRANSPOSE_UNIT_TEST
+
+#include <gpu/run.hpp>
+#include <mpi3/environment.hpp>
+
+#include <catch2/catch_all.hpp>
+
+TEST_CASE(INQ_TEST_FILE, INQ_TEST_TAG){
+
+  using namespace inq;
+  using namespace Catch::literals;
+  
+	parallel::communicator comm{boost::mpi3::environment::get_world_instance()};
+
+	SECTION("4x6 matrix") {
+		if(comm.size() == 2) {
+			
+			auto nx = 4;
+			auto ny = 6;
+			
+			auto partx = parallel::partition(nx, comm);
+			auto party = parallel::partition(ny, comm);	
+			
+			gpu::array<int, 2> matrix({nx, party.local_size()});
+			
+			if(comm.rank() == 0) {
+				matrix[0][0] =  1;
+				matrix[0][1] =  2;
+				matrix[0][2] =  3;
+				matrix[1][0] =  7;
+				matrix[1][1] =  8;
+				matrix[1][2] =  9;
+				matrix[2][0] = 13;
+				matrix[2][1] = 14;
+				matrix[2][2] = 15;
+				matrix[3][0] = 19;
+				matrix[3][1] = 20;
+				matrix[3][2] = 21;
+			}
+			
+			if(comm.rank() == 1) {
+				matrix[0][0] =  4;
+				matrix[0][1] =  5;
+				matrix[0][2] =  6;
+				matrix[1][0] = 10;
+				matrix[1][1] = 11;
+				matrix[1][2] = 12;
+				matrix[2][0] = 16;
+				matrix[2][1] = 17;
+				matrix[2][2] = 18;
+				matrix[3][0] = 22;
+				matrix[3][1] = 23;
+				matrix[3][2] = 24;
+			}
+			
+			parallel::transpose(comm, partx, party, matrix);
+			
+			CHECK(get<0>(sizes(matrix)) == party.size());
+			CHECK(get<1>(sizes(matrix)) == partx.local_size());	
+			
+			if(comm.rank() == 0) {
+				CHECK(matrix == gpu::array<int, 2>{
+						{1,  7},	
+						{2,  8},
+						{3,  9},
+						{4, 10},
+						{5, 11},
+						{6, 12}});
+			}
+			
+			if(comm.rank() == 1) {
+				CHECK(matrix == gpu::array<int, 2>{
+						{13, 19},	
+						{14, 20},
+						{15, 21},
+						{16, 22},
+						{17, 23},
+						{18, 24}});
+			}
+		}
+	}
+	
+}
+
+#endif
